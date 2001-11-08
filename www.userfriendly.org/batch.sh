7998f36c704e0e36d0a458ab39446ae8531ffa28
mitch <mitch>
2001-11-08 21:09:40 +0000

    wget mit Pfadangabe

--- www.userfriendly.org/batch.sh
+++ www.userfriendly.org/batch.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
-# $Id: batch.sh,v 1.2 2001-10-20 17:29:43 mitch Exp $
+# $Id: batch.sh,v 1.3 2001-11-08 21:09:40 mitch Exp $
 
 # $Log: batch.sh,v $
-# Revision 1.2  2001-10-20 17:29:43  mitch
+# Revision 1.3  2001-11-08 21:09:40  mitch
+# wget mit Pfadangabe
+#
+# Revision 1.2  2001/10/20 17:29:43  mitch
 # Meldung bei nicht existenter URL
 #
 # Revision 1.1  2001/10/20 17:22:17  mitch
 # Initial revision
 #
 
+WGET=/usr/local/bin/wget
 
-if [ -z $1 ]; then
+if [ -z ${1} ]; then
     echo "give DATE as [yyyymmdd]" 1>&2
     exit 1
 fi
 
 X=$1
 
-echo -n "getting $X: "
+echo -n "getting ${X}: "
 URL=http://ars.userfriendly.org/cartoons/?id=${X}
 PICURL=$(
-wget --use-proxy=OFF -O - ${URL} 2>/dev/null | \
-grep $X | grep "^<a href.*gif" | sed -e 's/gif.*$/gif/' -e 's/^.*src="//'
+${WGET} --use-proxy=OFF -O - ${URL} 2>/dev/null | \
+grep ${X} | grep "^<a href.*gif" | sed -e 's/gif.*$/gif/' -e 's/^.*src="//'
 )
-if [ -z $PICURL ]; then
+if [ -z ${PICURL} ]; then
     echo "wrong date?"
 else
-    wget --use-proxy=OFF -O ${X}.gif --referer=${URL} ${PICURL} 2>/dev/null
+    ${WGET} --use-proxy=OFF -O ${X}.gif --referer=${URL} ${PICURL} 2>/dev/null
     if [ -s ${X}.gif ]; then
 	echo "OK"
     else
