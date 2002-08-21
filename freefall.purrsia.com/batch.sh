945a0450774b65a0c657643c424a1c79b5baaba8
mitch <mitch>
2002-08-21 21:23:18 +0000

    Automatisches Herunter aller noch nicht geladenen Bilder auf einen Schlag

--- freefall.purrsia.com/batch.sh
+++ freefall.purrsia.com/batch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-# $Id: batch.sh,v 1.3 2001-12-22 13:34:39 mitch Exp $
+# $Id: batch.sh,v 1.4 2002-08-21 21:23:18 mitch Exp $
 
 # $Log: batch.sh,v $
-# Revision 1.3  2001-12-22 13:34:39  mitch
+# Revision 1.4  2002-08-21 21:23:18  mitch
+# Automatisches Herunter aller noch nicht geladenen Bilder auf einen Schlag
+#
+# Revision 1.3  2001/12/22 13:34:39  mitch
 # $FOLDER-Berechnung korrigiert
 #
 # Revision 1.2  2001/12/22 13:15:10  mitch
@@ -12,23 +15,19 @@
 # Initial revision
 #
 
-if [ -z $1 ]; then
-    echo "no FROM given" 1>&2
-    exit 1
+X=$(ls | egrep 'pic[0-9]{5}.gif' | tail -1 | cut -c 4-8)
+if [ -z ${X} ]; then
+    X=0  # first strip ever
 fi
 
-if [ -z $2 ]; then
-    echo "no TO given" 1>&2
-    exit 1
-fi
-
-echo "fetching from $1 to $2"
+echo "last fetched is $X"
 
 REFBASE="http://www.purrsia.com/freefall"
 GETBASE="http://www.purrsia.com/freefall"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 
-for X in $(seq -f %05g $1 $2); do
+while true ; do
+    X=$(printf %05d $(( 10#$X + 1)))
     FOLDER=ff$( echo $(( $( echo ${X} | sed 's/^0*//') +99 )) | cut -c 1 )00
     FILE=pic${X}.gif
     echo -n "fetching $X: "
@@ -39,7 +38,6 @@ for X in $(seq -f %05g $1 $2); do
     else
 	rm -f ${FILE}
 	echo "nok"
+	exit 0
     fi
 done
-
-echo "fini"
