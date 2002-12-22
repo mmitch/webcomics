82db3c370a26155dde402ae5b64a66924ac9a05b
mitch <mitch>
2002-12-22 21:41:36 +0000

    Wieder wie vorher, Teilbilder verhalten sich wie normale.

--- www.sexylosers.com/mkindex.sh
+++ www.sexylosers.com/mkindex.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 2002-12-22 12:53:52 mitch Exp $
+# $Id: mkindex.sh,v 1.3 2002-12-22 21:41:36 mitch Exp $
 
 # $Log: mkindex.sh,v $
-# Revision 1.2  2002-12-22 12:53:52  mitch
+# Revision 1.3  2002-12-22 21:41:36  mitch
+# Wieder wie vorher, Teilbilder verhalten sich wie normale.
+#
+# Revision 1.2  2002/12/22 12:53:52  mitch
 # Angepasse an Teilbilder
 #
 # Revision 1.1  2002/07/14 10:07:42  mitch
 # Initial revision
 #
 
-ls pic*.[gjh][ipt][fgm] | grep -v '^pic[0-9]*-' | sort | while read FILE; do
+ls pic*.[gj][ip][fg] | grep -v '^pic[0-9]*-' | sort | while read FILE; do
 
     echo -e "${FILE}\t[${FILE:3:3}]"
 
