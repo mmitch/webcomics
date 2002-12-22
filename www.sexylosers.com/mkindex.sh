40255c068c50fc06dbc0f1e8074fc8cbafc7bf49
mitch <mitch>
2002-12-22 12:53:52 +0000

    Angepasse an Teilbilder

--- www.sexylosers.com/mkindex.sh
+++ www.sexylosers.com/mkindex.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2002-07-14 10:07:42 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2002-12-22 12:53:52 mitch Exp $
 
 # $Log: mkindex.sh,v $
-# Revision 1.1  2002-07-14 10:07:42  mitch
+# Revision 1.2  2002-12-22 12:53:52  mitch
+# Angepasse an Teilbilder
+#
+# Revision 1.1  2002/07/14 10:07:42  mitch
 # Initial revision
 #
 
-ls pic*.[gj][ip][fg] | sort | while read FILE; do
+ls pic*.[gjh][ipt][fgm] | grep -v '^pic[0-9]*-' | sort | while read FILE; do
 
     echo -e "${FILE}\t[${FILE:3:3}]"
 
