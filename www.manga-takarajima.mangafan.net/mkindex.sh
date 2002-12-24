e20f1c71c1ee872fb51ed99edf44bec38bc168a9
mitch <mitch>
2002-12-24 12:47:16 +0000

    Initial revision.

---
+++ www.manga-takarajima.mangafan.net/mkindex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# $Id: mkindex.sh,v 1.1 2002-12-24 12:47:16 mitch Exp $
+
+# $Log: mkindex.sh,v $
+# Revision 1.1  2002-12-24 12:47:16  mitch
+# Initial revision.
+#
+
+echo "generate index by hand!  strip titles are included in the pictures"
