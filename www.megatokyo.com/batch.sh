396daa36673f9f68ee1d4a7882058012265aa538
mitch <mitch>
2003-01-13 17:51:49 +0000

    BufFix EXITCODE (Variable wurde in Subshell gesetzt -_-;)

--- www.megatokyo.com/batch.sh
+++ www.megatokyo.com/batch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-# $Id: batch.sh,v 1.7 2002-12-24 13:53:05 mitch Exp $
+# $Id: batch.sh,v 1.8 2003-01-13 17:51:49 mitch Exp $
 
 # $Log: batch.sh,v $
-# Revision 1.7  2002-12-24 13:53:05  mitch
+# Revision 1.8  2003-01-13 17:51:49  mitch
+# BufFix EXITCODE (Variable wurde in Subshell gesetzt -_-;)
+#
+# Revision 1.7  2002/12/24 13:53:05  mitch
 # `wget -q' statt `wget 2>/dev/null'
 #
 # Revision 1.6  2002/12/24 12:02:01  mitch
@@ -24,46 +27,50 @@
 # Initial revision
 #
 
-EXITCODE=2
-
 wget -qO - http://www.megatokyo.com \
 | grep "^<option value='.*</select>" \
 | sed -e "s/<\/select>$//" \
       -e "s/<\/option>/\\
 /g" \
 | perl batch.pl \
-| while read IDX; do
-    read DATE
-    read NR
-    read TITLE
-    read SPACER
-    if [ -s ${NR}.[gj][ip][fg] ]; then
-	echo "[$NR] skipped"
-    else
-	echo -n "[$NR]: fetching $DATE $TITLE   "
-	FILE=${NR}.gif
-	TEXT=${NR}.txt
-	wget -qO${FILE} --referer=http://www.megatokyo.com http://www.megatokyo.com/strips/${IDX}.gif
-	if [ -s ${FILE} ]; then
-	    echo "[$NR] $DATE $TITLE" > ${TEXT}
-	    echo "OK"
-	    EXITCODE=0
+| ( 
+    EXITCODE=2
+    while read IDX; do
+	read DATE
+	read NR
+	read TITLE
+	read SPACER
+	if [ -s ${NR}.[gj][ip][fg] ]; then
+	    echo "[$NR] skipped"
 	else
-	    rm -f ${FILE}
-	    # Try .jpg
-	    FILE=${NR}.jpg
-	    wget -qO${FILE} --referer=http://www.megatokyo.com http://www.megatokyo.com/strips/${IDX}.jpg
+	    echo -n "[$NR]: fetching $DATE $TITLE   "
+	    FILE=${NR}.gif
+	    TEXT=${NR}.txt
+	    wget -qO${FILE} --referer=http://www.megatokyo.com http://www.megatokyo.com/strips/${IDX}.gif
 	    if [ -s ${FILE} ]; then
 		echo "[$NR] $DATE $TITLE" > ${TEXT}
 		echo "OK"
 		EXITCODE=0
 	    else
 		rm -f ${FILE}
-		echo "failed!!!"
+	        # Try .jpg
+		FILE=${NR}.jpg
+		wget -qO${FILE} --referer=http://www.megatokyo.com http://www.megatokyo.com/strips/${IDX}.jpg
+		if [ -s ${FILE} ]; then
+		    echo "[$NR] $DATE $TITLE" > ${TEXT}
+		    echo "OK"
+		    EXITCODE=0
+		else
+		    rm -f ${FILE}
+		    echo "failed!!!"
+		fi
 	    fi
 	fi
-    fi
-done
+    done
+    exit ${EXITCODE}
+)
+
+EXITCODE=$?
 
 echo "fini"
 
