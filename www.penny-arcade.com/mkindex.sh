3ef0a23ae4766960f5e8e30250696bd589f0def1
mitch <mitch>
2003-02-08 20:09:58 +0000

    Datum wird im Titel mit angzeigt

--- www.penny-arcade.com/mkindex.sh
+++ www.penny-arcade.com/mkindex.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 1997-01-04 06:55:06 mitch Exp $
+# $Id: mkindex.sh,v 1.3 2003-02-08 20:09:58 mitch Exp $
 
 # $Log: mkindex.sh,v $
-# Revision 1.2  1997-01-04 06:55:06  mitch
+# Revision 1.3  2003-02-08 20:09:58  mitch
+# Datum wird im Titel mit angzeigt
+#
+# Revision 1.2  1997/01/04 06:55:06  mitch
 # Titel übernehmen, wenn vorhanden
 #
 # Revision 1.1  1997/01/04 02:32:45  mitch
@@ -15,7 +18,7 @@ ls *.[gj][ip][fg] | sort | while read FILE; do
     TITLEFILE=${FILE:0:8}.txt
     if [ -s ${TITLEFILE} ] ; then
 	read TEXT < ${TITLEFILE}
-	echo -e "${FILE}\t${TEXT}"
+	echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}: ${TEXT}"
     else
 	echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
     fi
