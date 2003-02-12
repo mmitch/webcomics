52e6234a558ad717c1717258032670e4736e42b4
ikari <ikari>
2003-02-12 00:42:25 +0000

    Ermittelt nun auch die Stripnamen. Kopierquelle: Megatokyo-Script Revision 1.6

--- www.penny-arcade.com/batch.sh
+++ www.penny-arcade.com/batch.sh
@@ -1,70 +1,69 @@
 #!/bin/bash
+# $Id: batch.sh,v 1.4 2003-02-12 00:42:25 ikari Exp $
+
+# $Log: batch.sh,v $
+# Revision 1.4  2003-02-12 00:42:25  ikari
+# Ermittelt nun auch die Stripnamen. Kopierquelle: Megatokyo-Script Revision 1.6
+#
+# Revision 1.6  2002/12/24 12:02:01  mitch
+# --use-proxy=off bei wget entfernt.
+#
+# Revision 1.5  2002/12/24 11:56:50  mitch
+# Ende mit RC=2, wenn kein neues Bild geladen wurde.
+#
+# Revision 1.4  2002/07/27 17:26:48  mitch
+# leere .gif-Dateien werden gelöscht
+#
+# Revision 1.3  2002/07/14 10:14:12  mitch
+# liest jetzt auch die .jpg-Dateien ein
+#
+# Revision 1.2  2002/01/25 15:43:45  mitch
+# Adapted to new Freshmeat page
+#
+# Revision 1.1  2001/10/20 19:04:50  mitch
+# Initial revision
+#
 
 EXITCODE=2
 
-LATEST=$(ls | egrep '[0-9]{8}.(gif|jpg)' | tail -1 | cut -c 1-8)
-if [ -z ${LATEST} ]; then
-    LATEST=19981118  # first strip ever
-fi
-
-YS=$(echo ${LATEST} | cut -c 1-4)
-MS=$(echo ${LATEST} | cut -c 5-6)
-DS=$(echo ${LATEST} | cut -c 7-8)
-
-YE=$(date +%Y)
-ME=$(date +%m)
-DE=$(date +%d)
-
-echo reading from ${YS}-${MS}-${DS} up to ${YE}-${ME}-${DE}
-
-PAGEBASE="http://www.penny-arcade.com/"
-PICBASE="http://www.penny-arcade.com/images/"
-USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
-
-YS=$(echo ${YS} | sed 's/^0*//')
-MS=$(echo ${MS} | sed 's/^0*//')
-DS=$(echo ${DS} | sed 's/^0*//')
-
-while true; do
-    DATE=$(printf %04d%02d%02d ${YS} ${MS} ${DS})
-    DATE2=$(printf %04d-%02d-%02d ${YS} ${MS} ${DS})
-
-    echo -n "fetching ${DATE}: "
-    EXT=gif
-    FILE=${DATE}.${EXT}
-    wget --user-agent="${USERAGENT}" --referer=${PAGEBASE}view.php?date=${DATE2} -O ${FILE} ${PICBASE}/${YS}/${DATE}l.${EXT} 2> /dev/null
-
-    if [ -s ${FILE} ]; then
-	echo OK
-	chmod -w ${FILE}
-	EXITCODE=0
+wget -O - http://www.penny-arcade.com/search.php 2>/dev/null \
+| grep "^<option value='.*</option>" \
+| sed -e "s/<\/select>$//" \
+      -e "s/<\/option>//"
+| perl batch.pl \
+| while read IDX; do
+    read DATE
+    read NR
+    read TITLE
+    read SPACER
+    if [ -s ${NR}.[gj][ip][fg] ]; then
+	echo "[$NR] skipped"
     else
-	test -w ${FILE} && rm ${FILE}
-	EXT=jpg
-	FILE=${DATE}.${EXT}
-        wget --user-agent="${USERAGENT}" --referer=${PAGEBASE}view.php?date=${DATE2} -O ${FILE} ${PICBASE}/${YS}/${DATE}l.${EXT} 2> /dev/null
+	echo -n "[$NR]: fetching $DATE $TITLE   "
+	FILE=${NR}.gif
+	TEXT=${NR}.txt
+	wget -O ${FILE} --referer=http://www.megatokyo.com http://www.megatokyo.com/strips/${IDX}.gif 2>/dev/null
 	if [ -s ${FILE} ]; then
-	    echo OK
-	    chmod -w ${FILE}
+	    echo "[$NR] $DATE $TITLE" > ${TEXT}
+	    echo "OK"
 	    EXITCODE=0
 	else
-	    test -w ${FILE} && rm ${FILE}
-	    echo nok
+	    rm -f ${FILE}
+	    # Try .jpg
+	    FILE=${NR}.jpg
+	    wget -O ${FILE} --referer=http://www.megatokyo.com http://www.megatokyo.com/strips/${IDX}.jpg 2>/dev/null
+	    if [ -s ${FILE} ]; then
+		echo "[$NR] $DATE $TITLE" > ${TEXT}
+		echo "OK"
+		EXITCODE=0
+	    else
+		rm -f ${FILE}
+		echo "failed!!!"
+	    fi
 	fi
     fi
-    
-    if [ ${DATE} = ${YE}${ME}${DE} ]; then
-	exit ${EXITCODE}
-    fi
-
-    DS=$((${DS} + 1))
-    if [ ${DS} -gt 31 ]; then
-	DS=1
-	MS=$((${MS} + 1))
-	if [ ${MS} -gt 12 ]; then
-	    MS=1
-	    YS=$((${YS} + 1))
-	fi
-    fi
-
 done
+
+echo "fini"
+
+exit ${EXITCODE}
