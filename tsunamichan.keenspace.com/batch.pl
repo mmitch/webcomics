389b2f16620cabd807d6affe9c91db4729803386
mitch <mitch>
2003-03-06 19:24:11 +0000

    Umgehen von Fehlern im Aufbar der Index-Seiten

--- tsunamichan.keenspace.com/batch.pl
+++ tsunamichan.keenspace.com/batch.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl -w
-# $Id: batch.pl,v 1.1 2003-03-06 19:04:00 mitch Exp $
+# $Id: batch.pl,v 1.2 2003-03-06 19:24:11 mitch Exp $
 
 # $Log: batch.pl,v $
-# Revision 1.1  2003-03-06 19:04:00  mitch
+# Revision 1.2  2003-03-06 19:24:11  mitch
+# Umgehen von Fehlern im Aufbar der Index-Seiten
+#
+# Revision 1.1  2003/03/06 19:04:00  mitch
 # Initial source import.
 # Kopierbasis: www.penny-arcade.com/batch.sh,v 1.1
 #
@@ -11,7 +14,7 @@ use strict;
 
 while (my $in = <>) {
     chomp $in;
-    if ($in =~ m,^<LI><A HREF=\"d/(\d{8})\.html\">(.*)</A>$,) {
+    if ( $in =~ m/^<li><a href=\"\/?d\/(\d{8})\.html\">(.*)<\/a>$/i ) {
 	printf "%s\n%s\n*-------*\n", $1, $2;
     }
 }
