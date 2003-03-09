11482f228abae45e96bd23b5c1b62c961ce8f60d
mitch <mitch>
2003-03-09 12:51:42 +0000

    Umgehung des Liner's Notes-Bugs in Anime_Parody #2

--- tsunamichan.keenspace.com/extract.pl
+++ tsunamichan.keenspace.com/extract.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
-# $Id: extract.pl,v 1.4 2003-03-09 09:03:42 mitch Exp $
+# $Id: extract.pl,v 1.5 2003-03-09 12:51:42 mitch Exp $
 #
 # $Log: extract.pl,v $
-# Revision 1.4  2003-03-09 09:03:42  mitch
+# Revision 1.5  2003-03-09 12:51:42  mitch
+# Umgehung des Liner's Notes-Bugs in Anime_Parody #2
+#
+# Revision 1.4  2003/03/09 09:03:42  mitch
 # Bugfix: nicht-leere Datei wurde auch bei im Fehlerfall angelegt
 #
 # Revision 1.3  2003/03/09 09:01:38  mitch
@@ -25,7 +28,7 @@ die "no date given" unless defined $date;
 
 # skip
 while (my $line=<>) {
-    if ($line =~ /<img.*alt=\"\".*src=\"\/comics\/$date.*<hr>/i) {
+    if ($line =~ /<img.*alt=\"\".*src=\"\/comics\/$date\./i) {
 	# Wenn wir das gefunden haben, erzeugen wir wenigstens eine nicht-leere
 	# Datei, damit nicht bei jedem Durchlauf erneut versucht wird, einen
 	# Text zu finden
