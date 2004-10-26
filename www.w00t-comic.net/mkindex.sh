9014a8d401bcd47dc831b1553569c32614c730e3
mitch <mitch>
2004-10-26 21:52:01 +0000

    support GIF and PNG

--- www.w00t-comic.net/mkindex.sh
+++ www.w00t-comic.net/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2004-10-26 21:41:29 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2004-10-26 21:52:01 mitch Exp $
 
-ls *.[gj][ip][fg] | sort | while read FILE; do
+ls *.[gp][in][fg] | sort | while read FILE; do
 
     echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
 
