f9b4a4d3375f66abf08dd5e7b03fbd1a7c9447c6
mitch <mitch>
2004-11-04 22:48:56 +0000

    copy base w00t/mkindex.sh,v 1.2

---
+++ www.crtaltdel-online.com/mkindex.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# $Id: mkindex.sh,v 1.1 2004-11-04 22:48:56 mitch Exp $
+
+ls *.[gj][ip][fg] | sort | while read FILE; do
+
+    echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
+
+done > index
