1a3c35aa31f62e0a4bdd5ea9760dfa8348d028fc
mitch <mitch>
2005-02-21 22:33:05 +0000

    Index mit Inhalt gefuellt Log entfernt (hier gibt es gar keine Zahlenbasis?!)

--- www.machall.com/mkindex.sh
+++ www.machall.com/mkindex.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2005-02-21 14:22:27 ikari Exp $
-
-# $Log: mkindex.sh,v $
-# Revision 1.1  2005-02-21 14:22:27  ikari
-# Wenn man nicht geduldig genug testet... Explizite Zahlenbasis fuer
-# arithmetische Expansion angegeben
-#
-# Revision 1.1  2002/07/14 10:05:07  mitch
-# Initial revision
-#
+# $Id: mkindex.sh,v 1.2 2005-02-21 22:33:05 mitch Exp $
 
 ls *.jpg | sort | while read FILE; do
 
-    echo -e "${FILE}"
+    echo -e "${FILE}\t#${FILE:0:5}"
 
 done > index
