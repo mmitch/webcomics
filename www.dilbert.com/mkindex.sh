ff45bec4e714c0fa84f29187d7c7ec12cd32e5d9
mitch <mitch>
2005-02-21 22:55:11 +0000

    ueberfluessige (), das sollte urspruenglich anders werden

--- www.dilbert.com/mkindex.sh
+++ www.dilbert.com/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 2005-02-21 22:38:22 mitch Exp $
+# $Id: mkindex.sh,v 1.3 2005-02-21 22:55:11 mitch Exp $
 
-(ls *.[jg][pi][gf]) | sort | while read FILE; do
+ls *.[jg][pi][gf] | sort | while read FILE; do
 
     echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
 
