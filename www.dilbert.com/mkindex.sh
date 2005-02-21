c9280b33bed66e5eaea23aba65876eb9b63ffcf1
mitch <mitch>
2005-02-21 22:38:22 +0000

    fertig

--- www.dilbert.com/mkindex.sh
+++ www.dilbert.com/mkindex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2005-02-21 22:35:38 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2005-02-21 22:38:22 mitch Exp $
 
-ls *.jpg | sort | while read FILE; do
+(ls *.[jg][pi][gf]) | sort | while read FILE; do
 
-    echo -e "${FILE}\t#${FILE:0:5}"
+    echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
 
 done > index
