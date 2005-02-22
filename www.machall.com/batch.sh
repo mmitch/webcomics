ab99667f63c9a879299af7c5c15202236e99e316
ikari <ikari>
2005-02-22 14:46:54 +0000

    Meldung korrigiert ("Fetching from 318 to 317"...) "abgebrochene Download nicht als leere Dateien in die Sammlung uebernehmen" von mitch geklaut ;)

--- www.machall.com/batch.sh
+++ www.machall.com/batch.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# $Id: batch.sh,v 1.3 2005-02-21 14:28:29 ikari Exp $
 
-# $Log: batch.sh,v $
-# Revision 1.3  2005-02-21 14:28:29  ikari
-# Versuch, die bisherigen Logeintraege reinbasteln zu lassen
-#
 EXITCODE=2
 
 LATEST=$(ls | egrep '[0-9]{5}.(gif|jpg)' | tail -1 | cut -c 1-5)
@@ -17,7 +12,7 @@ X=${LATEST}
 BASEURL="http://www.machall.com/"
 PICURL1="index.php?do_command=show_strip&strip_id="
 PICURL2="&auth=01101-10010-01010-10101-11111"
-
+TMP=deleteme.tmp
 CURRENT=$( wget -qO - ${BASEURL} \
          | grep show_strip   \
          | sed -e 's/.*id=//;s/&auth.*//')
@@ -25,22 +20,28 @@ CURRENT=$(printf %05d ${CURRENT})
 Y=$((10#${X}+1))
 Y=$(printf %05d ${Y})
 
-echo "Fetching from ${Y} to ${CURRENT}."
+if [ ${Y} -gt ${CURRENT} ]; then
+    echo "Up to date. Not fetching anything."
+    exit 1
+else
+    echo "Fetching from ${Y} to ${CURRENT}."
+fi
 
 while [ "${CURRENT}" -gt "${X}" ] ; do
 
     X=$((10#${X}+1))
     Y=$(printf %05d ${X})
     echo -n "getting ${Y}: "
-    URL="${BASEURL}?strip_id=${Y}"
-    PICURL="${BASEURL}${PICURL1}${Y}${PICURL2}"
-    wget -qO${Y}.jpg --referer=${URL} ${PICURL}
-    if [ -s ${Y}.jpg ]; then
+    URL="${BASEURL}?strip_id=${X}"
+    PICURL="${BASEURL}${PICURL1}${X}${PICURL2}"
+    wget -qO${TMP} --referer=${URL} ${PICURL}
+    if [ -s ${TMP} ]; then
+        mv ${TMP} ${Y}.jpg
         echo "OK"
         EXITCODE=0
     else
-        rm -f ${Y}.jpg
-        echo "fetch failed!!!"
+        rm -f ${TMP}
+        echo "fetch failed!"
         exit ${EXITCODE}
     fi
 
