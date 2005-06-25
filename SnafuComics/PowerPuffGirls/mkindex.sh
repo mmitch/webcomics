0f871a5011ad4ed554c3f05769d27bd041415721
mitch <mitch>
2005-06-25 17:20:41 +0000

    only .jpeg for now

--- SnafuComics/PowerPuffGirls/mkindex.sh
+++ SnafuComics/PowerPuffGirls/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2005-06-25 15:09:39 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2005-06-25 17:20:41 mitch Exp $
 
-ls *.jpg *.gif *.jpeg | sort | while read FILE; do
+ls *.jpeg | sort | while read FILE; do
 
     echo -e "${FILE}\t${FILE:4:2}.${FILE:2:2}.${FILE:0:2}"
 
