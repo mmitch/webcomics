665ecc1e208ff1ca4dd7700d17ea40a40af6d851
mitch <mitch>
2005-07-06 19:21:57 +0000

    change to tomoyo42's room

--- manga.clone-army.org/batch.sh
+++ manga.clone-army.org/batch.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
-# $Id: batch.sh,v 1.1 2005-07-06 19:21:17 mitch Exp $
+# $Id: batch.sh,v 1.2 2005-07-06 19:24:38 mitch Exp $
 
 EXITCODE=2
 
-LATEST=$(ls | egrep '[0-9]{6}.jpg' | tail -n 1 | cut -c 1-6 | sed 's/^0*//')
+LATEST=$(ls | egrep '[0-9]{6}.png' | tail -n 1 | cut -c 1-6 | sed 's/^0*//')
 if [ -z ${LATEST} ]; then
     LATEST=1  # first strip ever
 fi
 
 echo reading from ${LATEST}
 
-PAGEBASE="http://www.applegeeks.com/comic_archive/view.php?comic="
-PICBASE="http://www.applegeeks.com/comics/issue"
+PAGEBASE="http://manga.clone-army.org/t42r.php?page="
+PICBASE="http://manga.clone-army.org/t42r//tomoyo"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 
 while true; do
-    DATE=$(printf %06d ${LATEST})
+    DATE=$(printf %03d ${LATEST})
 
     echo -n "fetching ${DATE}: "
-    EXT=jpg
-    FILE=${DATE}.${EXT}
+    EXT=png
+    FILE=000${DATE}.${EXT}
 
     if [ -e ${FILE} -a ! -w ${FILE} ]; then
 	echo skipping
     else
 
-	wget --user-agent="${USERAGENT}" --referer=${PAGEBASE}${LATEST} -qO${FILE} ${PICBASE}${LATEST}.${EXT}
+	wget --user-agent="${USERAGENT}" --referer=${PAGEBASE}${LATEST} -qO${FILE} ${PICBASE}${DATE}.${EXT}
 	
 	if [ -s ${FILE} ]; then
 	    echo OK
--- manga.clone-army.org/mkindex.sh
+++ manga.clone-army.org/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2005-07-06 19:21:17 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2005-07-06 19:25:25 mitch Exp $
 
-ls *.[gj][ip][fg] | sort | while read FILE; do
+ls *.png | sort | while read FILE; do
 
     echo -e "${FILE}\t"`echo ${FILE:0:6} | sed 's/^0*//'`
 
