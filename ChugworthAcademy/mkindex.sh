d22d5a4bab383996f92d48e44697fd76eb9c5d05
mitch <mitch>
2005-07-10 15:26:19 +0000

    selten daemliche Dateinamen...

--- ChugworthAcademy/mkindex.sh
+++ ChugworthAcademy/mkindex.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2005-07-10 15:23:26 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2005-07-10 15:26:19 mitch Exp $
 
-ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
-    echo -e "${FILE}\t[${FILE:0:3}]"
-
+ls ??.jpeg ??.gif 2>/dev/null | sort | while read FILE; do
+    echo -e "${FILE}\t[${FILE:0:2}]"
 done > index
+
+ls ???.jpeg ???.gif 2>/dev/null | sort | while read FILE; do
+    echo -e "${FILE}\t[${FILE:0:3}]"
+done >> index
