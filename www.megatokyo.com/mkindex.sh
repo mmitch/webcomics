463e4b70b8e5e7db19d0b24b0d330c7a0228847b
mitch <mitch>
2006-02-13 21:50:27 +0000

    be more flexible with in-between-strips

--- www.megatokyo.com/mkindex.sh
+++ www.megatokyo.com/mkindex.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2002-07-14 10:16:58 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2006-02-13 21:50:27 mitch Exp $
 
 # $Log: mkindex.sh,v $
-# Revision 1.1  2002-07-14 10:16:58  mitch
+# Revision 1.2  2006-02-13 21:50:27  mitch
+# be more flexible with in-between-strips
+#
+# Revision 1.1  2002/07/14 10:16:58  mitch
 # Initial revision
 #
 
 ls *.[gj][ip][fg] | sort | while read FILE; do
 
-    read TEXT < ${FILE:0:3}.txt
+    NUMBER=${FILE%jpg}
+    NUMBER=${NUMBER%gif}
+    read TEXT < ${NUMBER}txt
     echo -e "${FILE}\t${TEXT}"
 
 done > index
