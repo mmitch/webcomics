0ba24c36024bffb67dea9c8df87ffd64947e83fa
mitch <mitch>
2006-08-27 10:06:35 +0000

    new comic Drow Tales (hint by /b/) hoffentlich klappt das mit der komischen Artikelsortierungsroutine...

---
+++ Drowtales/batch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# $Id: batch.sh,v 1.1 2006-08-27 10:06:35 mitch Exp $
+#
+# needs dosage from http://slipgate.za.net/dosage or Debian
+#
+mainline -b .. -c Drowtales
+
---
+++ Drowtales/mkindex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# $Id: mkindex.sh,v 1.1 2006-08-27 10:06:35 mitch Exp $
+
+ls *.jpeg *.dwf 2>/dev/null | perl -e '
+my @lines;
+while (my $line = <>) {
+    chomp $line;
+    if ($line =~ /^((\d{4})(\d\d)(\d\d))c(\d\d)(.*)\./) {
+        push @lines, {
+            "FILENAME" => $line,
+            "DATE_SORT" => "$1",
+            "DATE_DE" => "$4.$3.$2",
+            "CHAPTER" => $5,
+            "TITLE" => $6 
+        };        
+    } else {
+        warn $line;
+    }
+}
+my @sorted = sort {
+     $a->{CHAPTER} <=> $b->{CHAPTER}
+  || $a->{DATE_SORT} <=> $b->{DATE_SORT}
+  || $a->{TITLE} cmp $b->{TITLE}
+} @lines;
+foreach my $line (@sorted) {
+    printf "%s\t%s - Chapter %02d - %s\n",
+        $line->{FILENAME},
+        $line->{DATE_DE},
+        $line->{CHAPTER},
+        $line->{TITLE};
+}
+' > index
