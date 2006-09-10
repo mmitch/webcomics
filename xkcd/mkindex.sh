29b8ec4d09178f5b2c220833298e9a3fb15b9188
mitch <mitch>
2006-09-10 17:56:11 +0000

    show comic number in title

--- xkcd/mkindex.sh
+++ xkcd/mkindex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.3 2006-09-10 17:49:17 mitch Exp $
+# $Id: mkindex.sh,v 1.4 2006-09-10 17:56:11 mitch Exp $
 
 ls *.jpg *.png | sort | while read FILE; do
 
-    echo -e "${FILE}\t$(#(echo ${FILE:0:3} | sed 's/^0*//')) $(cat ${FILE%.???}.txt)"
+    echo -e "${FILE}\t#$(echo ${FILE:0:3} | sed 's/^0*//') $(cat ${FILE%.???}.txt)"
 
 done > index
