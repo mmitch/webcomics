94883c1ac2f5f41a174edc464bf1fef72a596dd2
mitch <mitch>
2006-09-10 17:49:17 +0000

    fix png support

--- xkcd/mkindex.sh
+++ xkcd/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 2006-09-10 17:48:38 mitch Exp $
+# $Id: mkindex.sh,v 1.3 2006-09-10 17:49:17 mitch Exp $
 
-ls *.jpg | sort | while read FILE; do
+ls *.jpg *.png | sort | while read FILE; do
 
     echo -e "${FILE}\t$(#(echo ${FILE:0:3} | sed 's/^0*//')) $(cat ${FILE%.???}.txt)"
 
