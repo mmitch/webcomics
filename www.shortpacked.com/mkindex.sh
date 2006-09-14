f6db880b2a651a9ab925cb7761f4148391637a06
mitch <mitch>
2006-09-14 08:03:32 +0000

    Wenn man PNG runterlädt, dann sollte es auch in den Index übernommen werden...

--- www.shortpacked.com/mkindex.sh
+++ www.shortpacked.com/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2006-09-10 20:27:08 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2006-09-14 08:03:32 mitch Exp $
 
-ls *.[gj][ip][fg] | sort | while read FILE; do
+ls *.[gjp][ipn][fg] | sort | while read FILE; do
 
     echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
 
