ba5dd3c50aa940cdeb812faa3876a0c37b752a55
mitch <mitch>
2006-11-16 22:39:09 +0000

    Vorbereitungen für Maxis großen Wunsch

--- www.ruthe.de/mkindex.sh
+++ www.ruthe.de/mkindex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2006-11-14 23:45:24 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2006-11-16 22:39:09 mitch Exp $
 
 ls *.gif *.jpg | sort | while read FILE; do
 
-    echo -e "${FILE}\t#${FILE:2:4}"
+    echo -e "${FILE}\thttp://www.ruthe.de/strip/strip.pl?fun=show&id=${FILE:2:4}\t#${FILE:2:4}"
 
 done > index
