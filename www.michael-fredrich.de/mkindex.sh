ecbae2db9061629966bafea27db57506284bb608
mitch <mitch>
2006-11-19 16:01:22 +0000

    add direct online links

--- www.michael-fredrich.de/mkindex.sh
+++ www.michael-fredrich.de/mkindex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 2006-10-15 15:48:56 mitch Exp $
+# $Id: mkindex.sh,v 1.3 2006-11-19 16:01:22 mitch Exp $
 
 ls *.gif *.jpg | sort | while read FILE; do
 
-    echo -e "${FILE}\t"$(cat ${FILE:0:6}.txt)
+    echo -e "${FILE}\thttp://www.michael-fredrich.de/Cartoons/${FILE:3:3}.htm\t"$(cat ${FILE:0:6}.txt)
 
 done > index
