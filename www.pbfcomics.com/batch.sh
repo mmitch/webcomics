769f26184153569539bacb78d8bedd296c64c681
mitch <mitch>
2006-11-20 22:20:00 +0000

    new comic, scripts based on www.megatokyo.com scripts

---
+++ www.pbfcomics.com/batch.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# $Id: batch.sh,v 1.1 2006-11-20 22:20:00 mitch Exp $
+
+STARTURL=http://www.pbfcomics.com
+
+wget -qO- ${STARTURL} \
+| egrep '<!--bold[0-9]+-->' \
+| sed -e 's/^.*href="//' -e 's:</a>.*::' -e 's/#/ /' -e 's/">/ /' \
+| tac \
+| ( 
+    EXITCODE=2
+    while read URL NUMBER TITLE; do
+	NUMBER=$(printf %04d $NUMBER)
+	FILENAME="${URL#*-}"
+	FILE=${NUMBER}-${FILENAME}
+	if [ -s "$FILE" ]; then
+	    echo "[$NR] skipped"
+	else
+	    echo -n "[$NUMBER]: fetching $TITLE   "
+	    TEXT=${NUMBER}-${FILENAME%.*}.txt
+	    wget -qO${FILE} --referer=${STARTURL} "${URL}"
+	    if [ -s "${FILE}" ]; then
+		echo "$URL [$NUMBER] $TITLE" > ${TEXT}
+		echo "OK"
+		EXITCODE=0
+	    else
+		rm -f ${FILE}
+		echo "failed!!!"
+	    fi
+	fi
+    done
+    exit ${EXITCODE}
+)
+
+EXITCODE=$?
+
+echo "fini"
+
+exit ${EXITCODE}
---
+++ www.pbfcomics.com/mkindex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# $Id: mkindex.sh,v 1.1 2006-11-20 22:20:00 mitch Exp $
+
+ls | egrep '\.(gif|jpg)$' | sort | while read FILE; do
+
+    read URL TEXT < ${FILE%%.*}.txt
+    echo -e "${FILE}\t${URL}\t${TEXT}"
+
+done > index
