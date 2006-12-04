b39cb6f93e54bcce2d822c021df9114d6bcffd74
mitch <mitch>
2006-12-04 10:40:37 +0000

    die blöde gif-Warnung nervt

--- www.ruthe.de/mkindex.sh
+++ www.ruthe.de/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 2006-11-16 22:39:09 mitch Exp $
+# $Id: mkindex.sh,v 1.3 2006-12-04 10:40:37 mitch Exp $
 
-ls *.gif *.jpg | sort | while read FILE; do
+ls *.gif *.jpg 2>/dev/null | sort | while read FILE; do
 
     echo -e "${FILE}\thttp://www.ruthe.de/strip/strip.pl?fun=show&id=${FILE:2:4}\t#${FILE:2:4}"
 
