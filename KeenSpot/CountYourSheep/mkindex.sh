af3e6cbde8f8842c89179852d558941132b8c938
mitch <mitch>
2006-12-31 14:00:46 +0000

    add direct online link

--- KeenSpot/CountYourSheep/mkindex.sh
+++ KeenSpot/CountYourSheep/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 2006-12-28 09:42:13 mitch Exp $
+# $Id: mkindex.sh,v 1.3 2006-12-31 14:00:46 mitch Exp $
 
 ls *.jpeg *.png *.gif 2>/dev/null | sort | while read FILE; do
-    echo -e "${FILE}\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
+    echo -e "${FILE}\thttp://www.countyoursheep.com/d/${FILE:0:8}.html\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
 
 done > index
