3f7c417193191ee7dc5027fe9bc7bcdf0e08d7dd
mitch <mitch>
2006-12-31 14:02:22 +0000

    add direct comic link

--- KeenSpot/SinFest/mkindex.sh
+++ KeenSpot/SinFest/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2006-12-27 17:41:21 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2006-12-31 14:02:22 mitch Exp $
 
 ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
-    echo -e "${FILE}\t[${FILE:13:2}.${FILE:10:2}.${FILE:5:4}]"
+    echo -e "${FILE}\thttp://www.sinfest.net/archive_page.php?comicID=${FILE:0:4}\t[${FILE:13:2}.${FILE:10:2}.${FILE:5:4}]"
 
 done > index
