6b6beaa11c566666f4a89875e3df6dce2ecc9529
mitch <mitch>
2006-12-31 14:16:55 +0000

    add direct online link

--- KeenSpot/RaveKitty/mkindex.sh
+++ KeenSpot/RaveKitty/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2006-02-04 18:01:05 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2006-12-31 14:16:55 mitch Exp $
 
 ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
-    echo -e "${FILE}\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
+    echo -e "${FILE}\thttp://ravekitty.comicgenesis.com/d/${FILE:0:8}.html\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
 
 done > index
--- KeenSpot/TheLounge/mkindex.sh
+++ KeenSpot/TheLounge/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2006-01-20 21:15:27 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2006-12-31 14:19:14 mitch Exp $
 
 ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
-    echo -e "${FILE}\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
+    echo -e "${FILE}\thttp://thelounge.comicgenesis.com/d/${FILE:0:8}.html\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
 
 done > index
