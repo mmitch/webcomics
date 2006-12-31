dbad688c91a62e25f01eadc5a8459134c4e3b1ee
mitch <mitch>
2006-12-31 14:15:27 +0000

    add direct link

--- KeenSpot/Ghastly/mkindex.sh
+++ KeenSpot/Ghastly/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2006-01-20 23:22:12 mitch Exp $
+# $Id: mkindex.sh,v 1.2 2006-12-31 14:15:27 mitch Exp $
 
 ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
-    echo -e "${FILE}\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
+    echo -e "${FILE}\thttp://www.ghastlycomic.com/d/${FILE:0:8}.html\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
 
 done > index
