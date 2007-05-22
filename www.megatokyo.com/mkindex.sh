ff4bb273044970238783b7c80d0913375a7d1b5e
mitch <mitch>
2007-05-22 17:27:10 +0000

    fixed for strips > 999
    (...und Piro ranted noch gross: "wir haben jetzt 4 Ziffern")

--- www.megatokyo.com/mkindex.sh
+++ www.megatokyo.com/mkindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.3 2006-02-13 21:51:50 mitch Exp $
+# $Id: mkindex.sh,v 1.4 2007-05-22 17:27:10 mitch Exp $
 
-ls *.[gj][ip][fg] | sort | while read FILE; do
+ls *.[gj][ip][fg] | sort -n | while read FILE; do
 
     NUMBER=${FILE%jpg}
     NUMBER=${NUMBER%gif}
