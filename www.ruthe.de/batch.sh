62739ffa8e9a14a16b9397f2dbdff224e057a36e
mitch <mitch>
2007-06-26 20:53:51 +0000

    ruthe-Skripte nach Webseitenumbau wieder funktionsfähig gemacht ich fürchte aber, dass die nur oberflächlich tun in der neuen Fassung hat der Kerl mehrere coppermine-galleries laufen und irgendwie weiß man nicht, woher nun die Bilder kommen
    Beispiel:
        http://www.ruthe.de/gallery/cpg1410/albums/userpics/10004/strip_0527.jpg
    vs. http://www.ruthe.de/gallery/cpg1410/albums/userpics/10001/strip_0527.jpg
    
    Was ist nun Strip 0527???

--- www.ruthe.de/batch.sh
+++ www.ruthe.de/batch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $Id: batch.sh,v 1.2 2006-11-19 15:15:48 mitch Exp $
+# $Id: batch.sh,v 1.3 2007-06-26 20:53:51 mitch Exp $
 # needs lynx
 
 EXITCODE=2
 
-PAGEBASE="http://www.ruthe.de/strip"
-PICBASE="$PAGEBASE/img/strip_"
+PAGEBASE="http://www.ruthe.de"
+PICBASE="$PAGEBASE/gallery/cpg1410/albums/userpics/10001/strip_"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 
 LATEST=$(ls | egrep '[0-9]{6}.(gif|jpg)' | tail -n 1 | cut -c 1-6 | sed 's/^0*//')
@@ -13,12 +13,14 @@ if [ -z ${LATEST} ]; then
     LATEST=1  # first strip ever
 fi
 
-MAXIMUM=$( wget --user-agent="${USERAGENT}" -qO- ${PAGEBASE}/strip.pl | \
-    egrep 'src="img/strip_[0-9]+\.jpg' | \
-    sed -e 's:^.*img/strip_0*::' -e 's/.jpg.*//' \
+MAXIMUM=$( wget --user-agent="${USERAGENT}" -qO- ${PAGEBASE}/ | \
+    egrep 'alt="strip_[0-9]+\.jpg' | \
+    sed -e 's:^.*alt="strip_0*::' -e 's/.jpg.*//' \
 )
 
 echo reading from ${LATEST} to ${MAXIMUM}
+
+[ -z ${MAXIMUM} ] && echo ruthe.batch: getting maximum failed && exit 1
 
 while [ ${LATEST} -le ${MAXIMUM} ] ; do
     DATE=$(printf %04d ${LATEST})
@@ -27,7 +29,7 @@ while [ ${LATEST} -le ${MAXIMUM} ] ; do
     EXT=jpg
     FILE=00${DATE}.${EXT}
     ANNEX=
-    HTML=${PAGEBASE}/strip.pl
+    HTML=${PAGEBASE}/gallery/cpg1410/displayimage.php
 
     if [ \( -e 00${DATE}.gif -a ! -w 00${DATE}.gif \) -o \( -e 00${DATE}.jpg -a ! -w 00${DATE}.jpg \) ]; then
 	echo skipping
--- www.ruthe.de/mkindex.sh
+++ www.ruthe.de/mkindex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.3 2006-12-04 10:40:37 mitch Exp $
+# $Id: mkindex.sh,v 1.4 2007-06-26 20:53:51 mitch Exp $
 
-ls *.gif *.jpg 2>/dev/null | sort | while read FILE; do
+ls *.jpg 2>/dev/null | sort | while read FILE; do
 
-    echo -e "${FILE}\thttp://www.ruthe.de/strip/strip.pl?fun=show&id=${FILE:2:4}\t#${FILE:2:4}"
+    echo -e "${FILE}\thttp://www.ruthe.de/gallery/cpg1410/albums/userpics/10001/strip_${FILE:2:4}.jpg\t#${FILE:2:4}"
 
 done > index
