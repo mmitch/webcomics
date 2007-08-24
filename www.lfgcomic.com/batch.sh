11c574aea8b5f2b4d57e9d97eef86711acf430d1
mitch <mitch>
2007-08-24 18:25:23 +0000

    initial source import based on batch.sh,v 1.11 and mkindex.sh,v 1.5 from xkcd.com

---
+++ www.lfgcomic.com/batch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# $Id: batch.sh,v 1.1 2007-08-24 18:25:23 mitch Exp $
+
+EXITCODE=2
+
+LATEST=$(ls | egrep '[0-9]{3}-.*.[jpg][pni][gf]' | tail -n 1 | cut -c 1-3 | sed 's/^0*//')
+if [ -z ${LATEST} ]; then
+    LATEST=1  # first strip ever
+fi
+
+echo reading from ${LATEST}
+
+PAGEBASE="http://www.lfgcomic.com/page/"
+PICBASE="http://archive.lfgcomic.com/"
+USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
+TMPFILE=./tmp.html
+
+while true; do
+
+    echo -n "fetching ${LATEST}: "
+    
+    HTMLURL="${PAGEBASE}${LATEST}"
+    if ! wget -q -O"${TMPFILE}" --user-agent="${USERAGENT}" "${HTMLURL}" ; then
+	echo nok
+	rm -f "$TMPFILE"
+	exit ${EXITCODE}
+    fi
+    FILENAME=$(grep "src=\"${PICBASE}" "${TMPFILE}" | sed -e "s|^.*${PICBASE}||" -e 's/\.jpg".*$/.jpg/' -e 's/\.png".*$/.png/' -e 's/\.gif".*$/.gif/')
+    
+    echo -n "${FILENAME} "
+    
+    FILE="$(printf "%03d-%s" "${LATEST}" "${FILENAME}")"
+
+    if [ -e ${FILE} -a ! -w ${FILE} ]; then
+	echo skipping
+    else
+
+	wget --user-agent="${USERAGENT}" --referer=${HTMLURL} -qO"${FILE}" "${PICBASE}${FILENAME}"
+	
+	if [ -s ${FILE} ]; then
+	    echo OK
+	    chmod -w ${FILE}
+	    EXITCODE=0
+	else
+	    test -w ${FILE} && rm ${FILE}
+	    echo nok
+	    exit ${EXITCODE}
+	fi
+    fi
+    
+    LATEST=$((${LATEST} + 1))
+
+done
+
+rm -f "$TMPFILE"
