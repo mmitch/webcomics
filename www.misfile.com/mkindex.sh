00d671799d3efcd7fcdab6e61f3318d61ca8a0f1
mitch <mitch>
2007-08-25 09:52:51 +0000

    add direct comic links

--- www.misfile.com/mkindex.sh
+++ www.misfile.com/mkindex.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.1 2007-08-25 09:30:47 ranma Exp $
+# $Id: mkindex.sh,v 1.2 2007-08-25 09:52:51 mitch Exp $
 
 ls *.jpg | sort | while read FILE; do
 
     STRIPZERO=$(echo ${FILE:2:3} | sed 's/^0*//')
-    echo -e "${FILE}\t#${STRIPZERO}"
+    echo -e "${FILE}\thttp://www.misfile.com/?page=${STRIPZERO}\t#${STRIPZERO}"
 
 done > index
