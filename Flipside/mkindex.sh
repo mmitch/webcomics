e64a1da978986fc35407f0c6ad9a9c78bb835a77
mitch <mitch>
2007-08-28 21:41:11 +0000

    fix sorting (use file date, they are OK after download) fix titles

--- Flipside/mkindex.sh
+++ Flipside/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ls *.gif *.jpeg | sort | while read FILE; do
+ls -rt *.gif *.jpeg | sort | while read FILE; do
 
-    echo -e "${FILE}\t${FILE:4:2}.${FILE:2:2}.${FILE:0:2}"
+    echo -e "${FILE}\t${FILE%.*}"
 
 done > index
