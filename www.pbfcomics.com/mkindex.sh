23ebd287d177ab1632d9a84cc98635e43f90caec
Christian Garbs <mitch@cgarbs.de>
2008-01-06 18:21:12 +0100

    fix index generation

--- www.pbfcomics.com/mkindex.sh
+++ www.pbfcomics.com/mkindex.sh
@@ -3,7 +3,7 @@
 
 ls | egrep '\.(gif|jpg)$' | sort | while read FILE; do
 
-    read URL TEXT < ${FILE%%.*}.txt
+    read URL TEXT < "${FILE%.*}.txt"
     echo -e "${FILE}\t${URL}\t${TEXT}"
 
 done > index
