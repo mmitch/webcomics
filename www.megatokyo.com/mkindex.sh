e61d2e55136749b5bc4c7406eba3e461eca9c852
Christian Garbs <mitch@cgarbs.de>
2008-05-04 12:55:10 +0200

    fix Megatokyo index file
    support PNG files
    update strip URLs
    remove CVS $Id$ tag

--- www.megatokyo.com/mkindex.sh
+++ www.megatokyo.com/mkindex.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.5 2007-11-24 21:47:06 mitch Exp $
 
-ls *.[gj][ip][fg] | sort -n | while read FILE; do
+ls *.[gjp][ipn][fg] | sort -n | while read FILE; do
 
     NUMBER=${FILE%.jpg}
     NUMBER=${NUMBER%.gif}
+    NUMBER=${NUMBER%.png}
     read TEXT < ${NUMBER}.txt
-    echo -e "${FILE}\thttp://www.megatokyo.com/index.php?strip_id=${NUMBER}\t${TEXT}"
+    echo -e "${FILE}\thttp://www.megatokyo.com/strip/${NUMBER}\t${TEXT}"
 
 done > index
