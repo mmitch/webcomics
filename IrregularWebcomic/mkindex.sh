d685ac3ccba68db34d50c2601e6cc71e350ce416
Christian Garbs <mitch@cgarbs.de>
2008-05-05 18:42:02 +0200

    add online URLs to Irregular Webcomic

--- IrregularWebcomic/mkindex.sh
+++ IrregularWebcomic/mkindex.sh
@@ -2,6 +2,8 @@
 # $Id: mkindex.sh,v 1.1 2005-04-26 19:30:15 mitch Exp $
 
 ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
-    echo -e "${FILE}\t[${FILE:5:4}]"
+
+    NR=$(echo ${FILE:5:4} | sed s/^0+//)
+    echo -e "${FILE}\thttp://www.irregularwebcomic.net/${NR}.html\t[${FILE:5:4}]"
 
 done > index
