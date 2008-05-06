628ddbad99f546e681cd8ec7381ebe2b2864f961
Christian Garbs <mitch@cgarbs.de>
2008-05-06 17:38:09 +0200

    add online URLs to Dilbert

--- www.dilbert.com/mkindex.sh
+++ www.dilbert.com/mkindex.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.3 2005-02-21 22:55:11 mitch Exp $
 
 ls *.[jg][pi][gf] | sort | while read FILE; do
 
-    echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
+    echo -e "${FILE}\thttp://www.dilbert.com/fast/${FILE:0:4}-${FILE:4:2}-${FILE:6:2}/\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
 
 done > index
