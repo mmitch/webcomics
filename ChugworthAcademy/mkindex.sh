64ad6852768522f96615ebc497cbfb60dc83f01b
Christian Garbs <mitch@cgarbs.de>
2008-08-17 18:09:31 +0200

    fix Chugworth Academy

--- ChugworthAcademy/mkindex.sh
+++ ChugworthAcademy/mkindex.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# $Id: mkindex.sh,v 1.2 2005-07-10 15:26:19 mitch Exp $
 
 ls ??.jpeg ??.gif 2>/dev/null | sort | while read FILE; do
     echo -e "${FILE}\t[${FILE:0:2}]"
@@ -7,4 +6,8 @@ done > index
 
 ls ???.jpeg ???.gif 2>/dev/null | sort | while read FILE; do
     echo -e "${FILE}\t[${FILE:0:3}]"
+done >> index
+
+ls ??????.jpeg ??????.gif 2>/dev/null | sort | while read FILE; do
+    echo -e "${FILE}\t[${FILE:2:2}.${FILE:4:2}.20${FILE:0:2}]"
 done >> index
