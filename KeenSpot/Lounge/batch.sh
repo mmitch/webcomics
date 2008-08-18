7a1f5a313ed9ed40f13859ceb5ec3cb72c98601a
Christian Garbs <mitch@cgarbs.de>
2008-08-18 22:01:48 +0200

    fix The Lounge rename

--- KeenSpot/Lounge/batch.sh
+++ KeenSpot/Lounge/batch.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-# $Id: batch.sh,v 1.1 2006-01-20 21:15:27 mitch Exp $
 #
 # needs dosage from http://slipgate.za.net/dosage or Debian
 #
-mainline -b ../.. -c KeenSpot/TheLounge
+mainline -b ../.. -c KeenSpot/Lounge
 
