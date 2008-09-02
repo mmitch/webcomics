378a284dead4eca42f1b392825d566d4229ba840
Andreas Geisenhainer <andy@kosmos.erebos.is-a-geek.org>
2008-09-02 20:19:22 +0200

    re-used the skript from SequentialArts to get LFGComic to work again.

--- www.lfgcomic.com/batch.sh
+++ www.lfgcomic.com/batch.sh
@@ -1,45 +1,32 @@
 #!/bin/bash
-# $Id: batch.sh,v 1.2 2007-08-24 18:30:53 mitch Exp $
 
 EXITCODE=2
 
-LATEST=$(ls | egrep '[0-9]{3}-.*.gif' | tail -n 1 | cut -c 1-3 | sed 's/^0*//')
+LATEST=$(ls | egrep '[0-9]{3}-lfg[0-9]{4}.gif' | tail -n 1 | cut -c 1-3 | sed 's/^0*//')
 if [ -z ${LATEST} ]; then
     LATEST=1  # first strip ever
 fi
 
 echo reading from ${LATEST}
-
+#echo "latte: $LATEST"
+#exit 1
 PAGEBASE="http://www.lfgcomic.com/page/"
-PICBASE="http://archive.lfgcomic.com/"
+PICBASE="http://www.lfgcomic.com/comics/"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
-TMPFILE=./tmp.html
-LASTFILENAME=$(ls $(printf %03d $LATEST)-*.gif | cut -c 5-)
 
 while true; do
 
     echo -n "fetching ${LATEST}: "
     
-    HTMLURL="${PAGEBASE}${LATEST}"
-    if ! wget -q -O"${TMPFILE}" --user-agent="${USERAGENT}" "${HTMLURL}" ; then
-	echo nok
-	rm -f "$TMPFILE"
-	exit ${EXITCODE}
-    fi
-    FILENAME=$(grep "src=\"${PICBASE}" "${TMPFILE}" | sed -e "s|^.*${PICBASE}||" -e 's/\.jpg".*$/.jpg/' -e 's/\.png".*$/.png/' -e 's/\.gif".*$/.gif/')
-    
+    FILENAME=$(printf lfg%04d.gif ${LATEST})
+
     echo -n "${FILENAME} "
-    
-    FILE="$(printf "%03d-%s" "${LATEST}" "${FILENAME}")"
+
+    FILE=$(printf %03d-lfg%04d.gif ${LATEST} ${LATEST})
 
     if [ -e ${FILE} -a ! -w ${FILE} ]; then
 	echo skipping
     else
-
-	if [ "${LASTFILENAME}" = "${FILENAME}" ] ; then
-	    echo "abort, filename unchanged (prevent endless download)"
-	    exit ${EXITCODE}
-	fi
 
 	wget --user-agent="${USERAGENT}" --referer=${HTMLURL} -qO"${FILE}" "${PICBASE}${FILENAME}"
 	
