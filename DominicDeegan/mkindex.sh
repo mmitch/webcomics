367c683dbe0b53e2e870248684ce240840dbba8e
Christian Garbs <mitch@cgarbs.de>
2008-09-11 11:07:25 +0200

    DominicDeegan: add direct online links

--- DominicDeegan/mkindex.sh
+++ DominicDeegan/mkindex.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#ls *.jpg *.gif *.jpeg | sort | while read FILE; do
 ls *.gif *.jpeg | sort | while read FILE; do
 
-    echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}";
+    echo -e "${FILE}\thttp://www.dominic-deegan.com/view.php?date=${FILE:0:4}-${FILE:4:2}-${FILE:6:2}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}";
 
 done > index
