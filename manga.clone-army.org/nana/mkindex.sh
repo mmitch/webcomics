b02a62d8bae52e829cb2eb8083d3ef7bdbff942a
Christian Garbs <mitch@cgarbs.de>
2008-11-07 23:41:03 +0100

    nana: re-add missing downloader
    warning: filenames have changed!  rename or re-download.

--- manga.clone-army.org/nana/mkindex.sh
+++ manga.clone-army.org/nana/mkindex.sh
@@ -2,7 +2,7 @@
 
 ls *.jpg | sort | while read FILE; do
 
-    NR=`echo ${FILE:10:3} | sed 's/^0*//'`    
+    NR=`echo ${FILE:0:6} | sed 's/^0*//'`    
     echo -e "${FILE}\thttp://manga.clone-army.org/nana.php?page=${NR}\t${NR}"
 
 done > index
