267fbbbbe98b4b49f42630e2955719830d63b08b
Christian Garbs <mitch@cgarbs.de>
2008-11-07 23:12:47 +0100

    manga.clone-army.org: generalize download routine

--- manga.clone-army.org/batch.sh
+++ manga.clone-army.org/batch.sh
@@ -13,30 +13,4 @@ PAGEBASE="http://manga.clone-army.org/t42r.php?page="
 PICBASE="http://manga.clone-army.org/t42r/tomoyo"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 
-while true; do
-    DATE=$(printf %03d ${LATEST})
-
-    echo -n "fetching ${DATE}: "
-    EXT=png
-    FILE=000${DATE}.${EXT}
-
-    if [ -e ${FILE} -a ! -w ${FILE} ]; then
-	echo skipping
-    else
-
-	wget --user-agent="${USERAGENT}" --referer=${PAGEBASE}${LATEST} -qO${FILE} ${PICBASE}${DATE}.${EXT}
-	
-	if [ -s ${FILE} ]; then
-	    echo OK
-	    chmod -w ${FILE}
-	    EXITCODE=0
-	else
-	    test -w ${FILE} && rm ${FILE}
-	    echo nok
-	    exit ${EXITCODE}
-	fi
-    fi
-    
-    LATEST=$((${LATEST} + 1))
-
-done
+source ./download.include
