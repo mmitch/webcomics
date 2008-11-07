81cdc8616c5e9faae5248ef69fb4725a4981c643
Christian Garbs <mitch@cgarbs.de>
2008-11-07 23:26:35 +0100

    manga.clone-army.org: variable filename extension

--- manga.clone-army.org/batch.sh
+++ manga.clone-army.org/batch.sh
@@ -12,5 +12,6 @@ echo reading from ${LATEST}
 PAGEBASE="http://manga.clone-army.org/t42r.php?page="
 PICBASE="http://manga.clone-army.org/t42r/tomoyo"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
+DEFEXT=png
 
 source ./download.include
