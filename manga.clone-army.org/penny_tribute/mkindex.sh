f8e159fb001609ff967dd18c1021cf0cacfec24d
Christian Garbs <mitch@cgarbs.de>
2008-11-07 23:51:27 +0100

    add Penny Tribute

---
+++ manga.clone-army.org/penny_tribute/mkindex.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ls *.jpg | sort | while read FILE; do
+
+    NR=`echo ${FILE:0:6} | sed 's/^0*//'`    
+    echo -e "${FILE}\thttp://manga.clone-army.org/penny.php?page=${NR}\t${NR}"
+
+done > index
