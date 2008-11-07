fe30316c3589cd23a1c7b798c44f9dcd3e2e9c18
Christian Garbs <mitch@cgarbs.de>
2008-11-07 23:34:02 +0100

    manga.clone-army.org: generalize last file detection routine

--- manga.clone-army.org/batch.sh
+++ manga.clone-army.org/batch.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-EXITCODE=2
+DEFEXT=png
 
-LATEST=$(ls | egrep '[0-9]{6}.png' | tail -n 1 | cut -c 1-6 | sed 's/^0*//')
-if [ -z ${LATEST} ]; then
-    LATEST=1  # first strip ever
-fi
-
-echo reading from ${LATEST}
+source ./start.include
 
 PAGEBASE="http://manga.clone-army.org/t42r.php?page="
 PICBASE="http://manga.clone-army.org/t42r/tomoyo"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
-DEFEXT=png
 
 source ./download.include
