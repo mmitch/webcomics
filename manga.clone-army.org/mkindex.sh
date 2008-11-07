d640564ae3ceb5c9113550b2bb3d22c2b9763523
Christian Garbs <mitch@cgarbs.de>
2008-11-07 23:22:37 +0100

    nana, tomoyo42: add direct online links

--- manga.clone-army.org/mkindex.sh
+++ manga.clone-army.org/mkindex.sh
@@ -2,6 +2,7 @@
 
 ls *.png | sort | while read FILE; do
 
-    echo -e "${FILE}\t"`echo ${FILE:0:6} | sed 's/^0*//'`
+    NR=`echo ${FILE:0:6} | sed 's/^0*//'`    
+    echo -e "${FILE}\thttp://manga.clone-army.org/t42r.php?page=${NR}\t${NR}"
 
 done > index
--- manga.clone-army.org/nana/mkindex.sh
+++ manga.clone-army.org/nana/mkindex.sh
@@ -2,6 +2,7 @@
 
 ls *.jpg | sort | while read FILE; do
 
-    echo -e "${FILE}\t"`echo ${FILE:10:3} | sed 's/^0*//'`
+    NR=`echo ${FILE:10:3} | sed 's/^0*//'`    
+    echo -e "${FILE}\thttp://manga.clone-army.org/nana.php?page=${NR}\t${NR}"
 
 done > index
