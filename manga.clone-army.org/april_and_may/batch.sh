9c8793ceb0dba331501dbc53b517f9c6ba3fe699
Christian Garbs <mitch@cgarbs.de>
2008-11-07 23:56:15 +0100

    add April and May

---
+++ manga.clone-army.org/april_and_may/batch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DEFEXT=jpg
+
+source ../start.include
+
+PAGEBASE='http://manga.clone-army.org/anm.php?page='
+PICBASE='http://manga.clone-army.org/AAM/AAM_'
+USERAGENT='Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]'
+
+source ../download.include
