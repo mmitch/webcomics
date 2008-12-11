09c44b97dddcb94ba8b0c4404fead58610d41d91
Christian Garbs <mitch@cgarbs.de>
2008-12-11 22:17:22 +0100

    Count your sheep: try a speedup in mkindex

--- KeenSpot/CountYourSheep/mkindex.sh
+++ KeenSpot/CountYourSheep/mkindex.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ls *.jpeg *.png *.gif 2>/dev/null | sort | while read FILE; do
+ls |  egrep '\.(jpeg|png|gif)$' | sort | while read FILE; do
     echo -e "${FILE}\thttp://www.countyoursheep.com/d/${FILE:0:8}.html\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
-
 done > index
