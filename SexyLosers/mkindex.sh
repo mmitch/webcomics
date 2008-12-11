91ec677078a68fed1cb7c27a352ceb3a005daa25
Christian Garbs <mitch@cgarbs.de>
2008-12-11 22:19:58 +0100

    SexyLosers: try a speedup on mkindex

--- SexyLosers/mkindex.sh
+++ SexyLosers/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls *.jpeg *.gif | sort | while read FILE; do
+ls | egrep '\.(jpeg|gif)$' | sort | while read FILE; do
 
     echo -e "${FILE}\thttp://www.sexylosers.com/${FILE:0:3}.html\t[${FILE:0:3}]"
 
