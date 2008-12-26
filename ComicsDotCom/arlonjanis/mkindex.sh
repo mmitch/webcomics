11dff6e80e2829a0f8ff328b18ca6c4fa2fb427d
Christian Garbs <mitch@cgarbs.de>
2008-12-26 19:20:47 +0100

    Arlo&Janis: try a speedup on mkindex

--- ComicsDotCom/arlonjanis/mkindex.sh
+++ ComicsDotCom/arlonjanis/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls *.jpeg *.gif | sort | while read FILE; do
+ls | egrep '\.(jpeg|gif)$' | sort | while read FILE; do
 
     echo -e "${FILE}\thttp://www.comics.com/comics/arlonjanis/archive/${FILE:0:19}.html\t[${FILE:17:2}.${FILE:15:2}.${FILE:11:4}]"
 
