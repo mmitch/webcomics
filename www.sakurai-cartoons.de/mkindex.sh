792853c8aa5aeaaa23e09992a653edead27148e5
Christian Garbs <mitch@cgarbs.de>
2008-12-26 20:49:38 +0100

    Sakurai: speed up mkindex
    dramatically!

--- www.sakurai-cartoons.de/mkindex.sh
+++ www.sakurai-cartoons.de/mkindex.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-ls *.gif | sort -n | while read FILE; do
+ls | grep '\.gif$' | sort -n | sed -e 's/^\(\(0*\([0-9]*\)\)\..*\)/\1 \2 \3/' | while read FILE NUMBER PARM; do
 
-    NUMBER=${FILE%.gif}
-    PARM="$(echo $NUMBER | sed 's/^0*//')"
     read TEXT < ${NUMBER}.txt
     echo -e "${FILE}\thttp://www.sakurai-cartoons.de/actual.php3?gross=${PARM}\t${TEXT}"
 
