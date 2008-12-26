7af449d6371804e19348eb78485ce4e4095869d8
Christian Garbs <mitch@cgarbs.de>
2008-12-26 20:41:55 +0100

    SinFest: speed up mkindex

--- KeenSpot/SinFest/mkindex.sh
+++ KeenSpot/SinFest/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
+ls | egrep '\.(jpeg|gif)$' | sort | while read FILE; do
     echo -e "${FILE}\thttp://www.sinfest.net/archive_page.php?comicID=${FILE:0:4}\t[${FILE:13:2}.${FILE:10:2}.${FILE:5:4}]"
 
 done > index
