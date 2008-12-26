8a8ca2da696c693a4cdf8bb91860c2d0246cc303
Christian Garbs <mitch@cgarbs.de>
2008-12-26 19:26:17 +0100

    Dominic Deegan: try speedup on mkindex

--- DominicDeegan/mkindex.sh
+++ DominicDeegan/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls *.gif *.jpeg | sort | while read FILE; do
+ls | egrep '\.(gif|jpeg)$' | sort | while read FILE; do
 
     echo -e "${FILE}\thttp://www.dominic-deegan.com/view.php?date=${FILE:0:4}-${FILE:4:2}-${FILE:6:2}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}";
 
