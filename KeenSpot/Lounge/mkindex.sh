5060274c41fe98ef58dda0d28fd083aea717bf38
Christian Garbs <mitch@cgarbs.de>
2008-12-26 20:41:02 +0100

    The Lounge: speed up mkindex

--- KeenSpot/Lounge/mkindex.sh
+++ KeenSpot/Lounge/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls *.jpeg *.gif 2>/dev/null | sort | while read FILE; do
+ls | egrep '\.(jpeg|gif)$' | sort | while read FILE; do
     echo -e "${FILE}\thttp://thelounge.comicgenesis.com/d/${FILE:0:8}.html\t[${FILE:6:2}.${FILE:4:2}.${FILE:0:4}]"
 
 done > index
