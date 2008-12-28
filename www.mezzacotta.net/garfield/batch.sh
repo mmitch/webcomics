f3ac7dcc4397fbe07a16ae8c07c083b40c67b1a1
Christian Garbs <mitch@cgarbs.de>
2008-12-28 18:48:10 +0100

    add Square Root of Minus Garfield

---
+++ www.mezzacotta.net/garfield/batch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DEFEXT=png
+
+source ../start.include
+
+PAGEBASE="http://www.mezzacotta.net/garfield/?comic="
+PICBASE="http://www.mezzacotta.net/garfield/comics/"
+USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
+
+source ../download.include
