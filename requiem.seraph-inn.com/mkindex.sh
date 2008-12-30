418c2545917b4a48bacdc81c72f29c504660ab2e
Christian Garbs <mitch@cgarbs.de>
2008-12-30 19:21:28 +0100

    Phoenix Requiem: speed up mkindex

--- requiem.seraph-inn.com/mkindex.sh
+++ requiem.seraph-inn.com/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls *.jpg | sort | while read FILE; do
+ls | grep '\.jpg$' | sort | sed -e 's/\(0*\([0-9]*\)\.jpg\)/\1 \2/' | while read FILE STRIPZERO; do
 
     STRIPZERO=$(echo ${FILE:0:3} | sed 's/^0*//')
     echo -e "${FILE}\thttp://requiem.seraph-inn.com/viewcomic.php?page=${STRIPZERO}\t#${STRIPZERO}"
