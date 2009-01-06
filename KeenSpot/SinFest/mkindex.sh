96e9874a23100e8b8fa0778088fd5d967703a558
Christian Garbs <mitch@cgarbs.de>
2009-01-06 08:28:39 +0100

    SinFest: fix mkindex

--- KeenSpot/SinFest/mkindex.sh
+++ KeenSpot/SinFest/mkindex.sh
@@ -2,6 +2,6 @@
 
 ls | egrep '\.(jpeg|gif)$' | sort | sed -e 's/\(\([0-9]*\)-\([0-9]*\)-\([0-9]*\)-\([0-9]*\)\..*\)/\1 \2 \5.\4.\3/' | while read FILE NR DATE; do
 
-    echo "${FILE}\thttp://www.sinfest.net/archive_page.php?comicID=${NR}\t[${DATE}]"
+    echo -e "${FILE}\thttp://www.sinfest.net/archive_page.php?comicID=${NR}\t[${DATE}]"
 
 done > index
