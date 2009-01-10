581854d911bccf591e143ae361bc39a4748156b8
Christian Garbs <mitch@cgarbs.de>
2009-01-10 18:31:32 +0100

    Irregular Webcomic: display missing png images
    also refine comic titles on duplicate numbers

--- IrregularWebcomic/mkindex.sh
+++ IrregularWebcomic/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls | egrep '\.(jpeg|gif)$' | sort | sed -e 's/^\([^0-9]*0*\([0-9]*\).*\)$/\1 \2/' | while read FILE NR; do
+ls | egrep '\.(jpeg|gif|png)$' | sort | sed -e 's/^\([^0-9]*0*\([0-9a-zA-Z]*\).*\)$/\1 \2/' | while read FILE NR; do
 
     echo -e "${FILE}\thttp://www.irregularwebcomic.net/${NR}.html\t[${NR}]"
 
