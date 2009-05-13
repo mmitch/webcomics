12c03f5ee1f9d9d55590396f176485067dd3470c
Christian Garbs <mitch@cgarbs.de>
2009-05-13 23:43:54 +0200

    fix Square Root of Minus Garfield
    new: .gif additional to .png

--- www.mezzacotta.net/garfield/mkindex.sh
+++ www.mezzacotta.net/garfield/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls | grep \.png$ | sort | sed -e 's/^\(0*\([0-9]*\)\..*$\)/\1 \2/' | while read FILE NR; do
+ls | grep -E '\.(png|gif)$' | sort | sed -e 's/^\(0*\([0-9]*\)\..*$\)/\1 \2/' | while read FILE NR; do
 
     echo -e "${FILE}\thttp://www.mezzacotta.net/garfield/?comic=${NR}\t${NR}: $( cat ${FILE}.text)"
 
