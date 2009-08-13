4a16f30952993f3fd3f142fac9423ce79bcffe7c
Christian Garbs <mitch@cgarbs.de>
2009-08-13 17:10:06 +0200

    VGButts: fix removal of leading zeroes in index

--- VGButts/mkindex.sh
+++ VGButts/mkindex.sh
@@ -3,7 +3,8 @@
 ls *.jpg | sort -n | while read FILE; do
 
     NUMBER=${FILE%.jpg}
+    NUM=$(( 1${NUMBER} - 1000000 ))
     read TEXT < ${NUMBER}.txt
-    echo -e "${FILE}\thttp://orangebox.wwoec.com/vgbutts/ep/${NUMBER##0*}\t${TEXT}"
+    echo -e "${FILE}\thttp://orangebox.wwoec.com/vgbutts/ep/${NUM}\t${TEXT}"
 
 done > index
