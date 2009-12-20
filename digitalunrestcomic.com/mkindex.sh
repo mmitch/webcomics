ac4ed12df07d529ae46f51a591af413a370e05a5
Christian Garbs <mitch@cgarbs.de>
2009-12-20 18:49:50 +0100

    DU: fix online link

--- digitalunrestcomic.com/mkindex.sh
+++ digitalunrestcomic.com/mkindex.sh
@@ -3,6 +3,6 @@
 ls *.jpg | sort -n | while read FILE; do
 
     DATE=${FILE:0:4}-${FILE:4:2}-${FILE:6:2}
-    echo -e "${FILE}\thttp://digitalunrestcomic.com/strips/${DATE}.jpg\t${DATE}"
+    echo -e "${FILE}\thttp://digitalunrestcomic.com/index.php?date=${DATE}\t${DATE}"
 
 done > index
