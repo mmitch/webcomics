185339166560a8e6aaa74bfe1dbc7fb9f5d6d411
Benjamin Heuer <benjaminheuer@t-online.de>
2010-06-02 14:25:35 +0200

    Move common code to a perl module.

---
+++ Webcomic.pm
@@ -0,0 +1,87 @@
+package Webcomic;
+
+use warnings;
+use strict;
+
+use LWP::UserAgent;
+use HTML::TokeParser;
+
+BEGIN {
+    use Exporter   ();
+    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+    $VERSION     = 1.00;
+
+    @ISA         = qw(Exporter);
+    @EXPORT      = qw(&get_comics &file_exists &tag_property &basename);
+    %EXPORT_TAGS = ( );
+
+    @EXPORT_OK   = qw();
+}
+our @EXPORT_OK;
+
+sub download {
+    my ($info, $ua, $exitcode) = @_;
+    $| = 1;
+    print "fetching $info->{'filename'}: ";
+    my $res = $ua->mirror($info->{'image'}, $info->{'filename'});
+    if ($res->is_success()) {
+        print "ok\n";
+        $exitcode = 0;
+    } else {
+        print "nok\n";
+    }
+}
+
+sub get_comics {
+    my ($next, $tags, $end) = @_;
+    my $exitcode = 2;
+    my $ua = LWP::UserAgent->new;
+    my %info;
+    while (1) {
+        my $res = $ua->get($next);
+        unless ($res->is_success) {
+            die "Could not download URL $next: ", $res->status_line, ".\n";
+        }
+        my $parser = HTML::TokeParser->new(\$res->decoded_content());
+        while (my $tag = $parser->get_tag(keys %$tags)) {
+            $tags->{$tag->[0]}->($tag, $parser, \%info, $ua);
+            last if ($info{'end'});
+        }
+        $info{'end'} = 0;
+        if ($end->(%info)) {
+            print "Finished.\n";
+            exit $exitcode;
+        }
+        download(\%info, $ua, \$exitcode);
+        $next = $info{'next'};
+    }
+}
+
+sub tag_property {
+    my ($tag, $property, $value, $contains) = @_;
+    if (defined($tag->[1]->{$property})) {
+        if (defined($contains)) {
+            return ($tag->[1]->{$property} =~ /$value/);
+        } else {
+            return ($tag->[1]->{$property} eq $value);
+        }
+    } else {
+        return 0;
+    }
+}
+
+
+sub file_exists {
+    my %info = @_;
+    return (-e $info{'filename'});
+}
+
+sub basename {
+    my $image = shift;
+    return (split(/.*\//, $image))[1];
+}
+
+END { }
+
+1;
--- www.dorktower.com/batch.pl
+++ www.dorktower.com/batch.pl
@@ -1,54 +1,32 @@
 #!/usr/bin/perl -w
 
 use strict;
-
-use LWP::UserAgent;
-use HTML::TokeParser;
+use lib '..';
+use Webcomic;
 
 my $url = 'http://www.dorktower.com/';
-my $next = $url;
-my $ua = LWP::UserAgent->new;
-my $exitcode = 2;
-my ($y, $m, $d);
 
-while (1) {
-    my $res = $ua->get($next);
-    unless ($res->is_success) {
-        die "Could not download URL $next.";
-    }
-    my $parser = HTML::TokeParser->new(\$res->decoded_content());
-    while (my $tag = $parser->get_tag('div', 'a')) {
-        if ($tag->[0] eq 'div') {
-            if (defined($tag->[1]->{'class'})
-                and $tag->[1]->{'class'} eq 'entry') {
-                my $image = $parser->get_tag('img')->[1]->{'src'};
-                unless ($image =~ /^http/) {
-                    $image = $url . $image;
-                }
-                my ($orig) = ($image =~ /.*\/(.*)/);
-                my $filename = "$y$m$d-$orig";
-                if (-e $filename and ($filename !~ /797|DTcartoon/)) {
-                    print "Finished.\n";
-                    exit $exitcode;
-                }
-                $| = 1;
-                print "fetching $filename: ";
-                $res = $ua->mirror($image, $filename);
-                if ($res->is_success()) {
-                    print "ok\n";
-                    $exitcode = 0;
-                } else {
-                    print "nok\n";
-                }
-                last;
-            }
-        } elsif ($tag->[0] eq 'a') {
-            if (defined($tag->[1]->{'title'})
-                and ($tag->[1]->{'title'} =~ /(DORK TOWER)|(Permanent Link)/)) {
-                ($y, $m, $d) = ($tag->[1]->{'href'} =~ /(\d{4})\/(\d{2})\/(\d{2})/);
-            } elsif ($parser->get_text() eq '< - Previous') {
-                $next = $url . $tag->[1]->{'href'};
-            }
-        }
-    }
-}
+get_comics($url,
+           { 'a' => sub { my ($tag, $parser, $info) = @_;
+                          if (tag_property($tag, 'title', '(DORK TOWER)|(Permanent Link)', 1)) {
+                              my ($y, $m, $d) = ($tag->[1]->{'href'} =~ /(\d{4})\/(\d{2})\/(\d{2})/);
+                              $info->{'date'} = "$y$m$d";
+                          } elsif ($parser->get_text() eq '< - Previous') {
+                              $info->{'next'} = $url . $tag->[1]->{'href'};
+                          }
+                      },
+             'div' => sub { my ($tag, $parser, $info) = @_;
+                            if (tag_property($tag, 'class', 'entry')) {
+                                $info->{'image'} = $parser->get_tag('img')->[1]->{'src'};
+                                unless ($info->{'image'} =~ /^http/) {
+                                    $info->{'image'} = $url . $info->{'image'};
+                                }
+                                my ($orig) = ($info->{'image'} =~ /.*\/(.*)/);
+                                $info->{'filename'} = $info->{'date'}."-$orig";
+                                $info->{'end'} = 1;
+                            }
+                        }
+           },
+           sub { my %info = @_;
+                 return (-e $info{'filename'} and ($info{'filename'} !~ /797|DTcartoon/))
+             });
