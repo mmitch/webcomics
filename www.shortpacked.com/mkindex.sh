09281dea3d29ba537a354511b128d37b37e31a1c
Christian Garbs <mitch@cgarbs.de>
2010-11-27 19:29:21 +0100

    add strip name to Shortpacked

--- www.shortpacked.com/mkindex.sh
+++ www.shortpacked.com/mkindex.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ls | egrep '\.(jpg|png|gif)$' | sort | while read FILE; do
+ls *.png | sort | while read FILE; do
 
-    echo -e "${FILE}\t${FILE:8:2}.${FILE:5:2}.${FILE:0:4}"
+    NAME=${FILE:11}
+    echo -e "${FILE}\t${FILE:8:2}.${FILE:5:2}.${FILE:0:4} ${NAME%.*}"
 
 done > index
