4157d282a4a31a4c8902b08a98593b7c6d8b52fa
Christian Garbs <mitch@cgarbs.de>
2011-01-15 00:31:57 +0100

    add .png, only rotate when needed

--- mkpdf.sh
+++ mkpdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-convert -rotate 270 \
-$(ls | egrep '\.(gif|jpe?g)$' | sort) \
+convert -rotate \>270 \
+$(ls | egrep '\.(gif|jpe?g|png)$' | sort) \
 $(grep ^NAME COMIC | sed -e 's/^NAME: //' | tr -cd '[a-zA-Z0-9]').pdf
 
