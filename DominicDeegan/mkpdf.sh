52de825f12bb2a555cb6e3b93eecbe3f0e5e0fff
Christian Garbs <mitch@cgarbs.de>
2011-01-14 23:44:08 +0100

    better PDF conversion via ImageMagick

--- DominicDeegan/mkpdf.sh
+++ DominicDeegan/mkpdf.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-LATEX=file.latex
-
-cat > $LATEX <<EOF
-\documentclass{minimal}
-\usepackage{graphicx}
-\begin{document}
-EOF
-
-ls | egrep '\.(gif|jpe?g)$' | sort | while read FILE; do
-
-    echo $FILE >&2
-
-    case $FILE in
-	*.gif)
-	    NEWFILE=tmp_${FILE%.gif}.png
-	    [ -e $NEWFILE ] || convert $FILE $NEWFILE
-	    FILE=$NEWFILE
-	    ;;
-    esac	    
-
-    echo "\includegraphics[scale=0.8, angle=90]{$FILE}\\\\"
-#    echo "\clearpage"
-
-done >> $LATEX
-
-
-cat >> $LATEX <<EOF
-\end{document}
-EOF
+convert -rotate 270 \
+$(ls | egrep '\.(gif|jpe?g)$' | sort) \
+$(grep ^NAME COMIC | sed -e 's/^NAME: //' | tr -cd '[a-zA-Z0-9]').pdf
 
