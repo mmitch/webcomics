3f86a298a51e6b7b21a2758d94da98804be3f68d
Christian Garbs <mitch@cgarbs.de>
2011-01-30 18:23:18 +0100

    better comic titles on Scandinavia and the World

--- satwcomic.com/mkindex.sh
+++ satwcomic.com/mkindex.sh
@@ -3,6 +3,9 @@
 ls *.jpg | sort -n | while read FILE; do
 
     DATE=${FILE:0:8}
-    echo -e "${FILE}\t${DATE:6:2}.${DATE:4:2}.${DATE:0:4}"
+    TEXT=${FILE:9}
+    TEXT=${TEXT%.jpg}
+    TEXT=${TEXT//-/ }
+    echo -e "${FILE}\t${DATE:6:2}.${DATE:4:2}.${DATE:0:4} ${TEXT}"
 
 done > index
