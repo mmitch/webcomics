ca5e71c9abc87abc3153d05bb5e7a5162fc368f8
Christian Garbs <mitch@cgarbs.de>
2011-06-15 20:48:21 +0200

    Questionable Content: remove bashism

--- QuestionableContent/batch.sh
+++ QuestionableContent/batch.sh
@@ -2,10 +2,10 @@
 
 DEFEXT=png
 
-source ./start.include
+. ./start.include
 
 PAGEBASE="http://questionablecontent.net/view.php?comic="
 PICBASE="http://questionablecontent.net/comics/"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 
-source ./download.include
+. ./download.include
