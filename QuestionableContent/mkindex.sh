e8c8517c466357e677583d7f5937f209df3d9690
Christian Garbs <mitch@cgarbs.de>
2011-10-04 19:22:53 +0200

    fix Questionable Content
    new file extension: .jpg

--- QuestionableContent/mkindex.sh
+++ QuestionableContent/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls | egrep '\.(gif|png)$' | sort | sed -e 's/^\(0*\([0-9]*\)\..*$\)/\1 \2/' | while read FILE STRIPZERO; do
+ls | egrep '\.(gif|png|jpg)$' | sort | sed -e 's/^\(0*\([0-9]*\)\..*$\)/\1 \2/' | while read FILE STRIPZERO; do
 
     echo -e "${FILE}\thttp://questionablecontent.net/view.php?comic=${STRIPZERO}\t#${STRIPZERO}"
 
