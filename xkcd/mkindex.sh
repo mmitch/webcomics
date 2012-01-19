6494f5e2c6c722fe3381865de09cbc20599331de
Christian Garbs <mitch@cgarbs.de>
2012-01-19 21:19:08 +0100

    xkcd: fix index > 999

--- xkcd/mkindex.sh
+++ xkcd/mkindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls | egrep '\.(jpg|png|gif)$' | sort | sed -e 's/\(\(0*\([0-9]*\).*\)\..*\)/\1 \2 \3/' | while read FILE NAME STRIPZERO; do
+ls | egrep '\.(jpg|png|gif)$' | sort -n | sed -e 's/\(\(0*\([0-9]*\).*\)\..*\)/\1 \2 \3/' | while read FILE NAME STRIPZERO; do
 
     echo -e "${FILE}\thttp://xkcd.com/c${STRIPZERO}.html\t#${STRIPZERO} $(cat ${NAME}.txt)"
 
