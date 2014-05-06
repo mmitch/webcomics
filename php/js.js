38fb2c3e0b7e336d7797df706be9d30a03b9b9a2
Christian Garbs <mitch@cgarbs.de>
2014-05-06 21:51:46 +0200

    prettify JavaScript

--- php/js.js
+++ php/js.js
@@ -1,12 +1,25 @@
+// Copyright (C) 2002-2008,2010,2014   Christian Garbs <mitch@cgarbs.de>
+// licensed under GNU GPL v3 or later
+
 function keypress(e)
 {
-        var keynum;
-        if(e.which) { keynum = e.which; }// Netscape/Firefox/Opera
-        else
-        if(window.event) { keynum = e.keyCode; } // IE
+    var keynum;
 
-        if (keynum == 37) { window.location = document.getElementById("linkprev").href; }
-        else
-        if (keynum == 39) { window.location = document.getElementById("linknext").href; }
+    if (e.which) // Netscape/Firefox/Opera
+    {
+	keynum = e.which;
+    }
+    else if (window.event) // IE
+    {
+	keynum = e.keyCode;
+    } 
+
+    if (keynum == 37)
+    {
+	window.location = document.getElementById("linkprev").href;
+    }
+    else if (keynum == 39)
+    {
+	window.location = document.getElementById("linknext").href;
+    }
 }
-
