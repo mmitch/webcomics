eea2973a4846add13b634249ed4704c1c93c3254
Christian Garbs <mitch@cgarbs.de>
2014-08-26 18:04:24 +0200

    fix Looking for Group

--- www.lfgcomic.com/batch.sh
+++ www.lfgcomic.com/batch.sh
@@ -2,7 +2,7 @@
 
 EXITCODE=2
 
-LATEST=$(ls | egrep '[0-9]{3}-lfg[0-9]{4}.gif' | tail -n 1 | cut -c 1-3 | sed 's/^0*//')
+LATEST=$(ls | egrep '[0-9]{3}[0-9]*-lfg[0-9]{4}.gif' | tail -n 1 | cut -d- -f 1  | sed 's/^0*//')
 if [ -z ${LATEST} ]; then
     LATEST=1  # first strip ever
 fi
@@ -10,7 +10,6 @@ fi
 echo reading from ${LATEST}
 
 PAGEBASE="http://www.lfgcomic.com/page/"
-PICBASE="http://www.lfgcomic.com/uploads/comics/"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 
 while true; do
@@ -26,18 +25,28 @@ while true; do
     if [ -e ${FILE} -a ! -w ${FILE} ]; then
 	echo skipping
     else
-	SOURCE_FILE=`lynx --dump ${PAGEBASE}${LATEST}|grep .gif|perl -p -e s'/[\[\] ]//g'`
-	wget --user-agent="${USERAGENT}" --referer=${HTMLURL} -qO"${FILE}" "${PICBASE}${SOURCE_FILE}"
+
+	# WTF - jumping page numbers?!
+	case ${LATEST} in
+	    647)
+		NUMBER=2967
+		;;
+	    *)
+		NUMBER=${LATEST}
+		;;
+	esac
+
+	HTMLURL=${PAGEBASE}${NUMBER}/
+	SOURCE_FILE="$(wget -qO- --user-agent="${USERAGENT}" ${HTMLURL} | grep -A1 '<div id="comic">' | tail -n 1 | sed -e 's/^.*src="//' -e 's/".*//')"
+	wget --user-agent="${USERAGENT}" --referer=${HTMLURL} -qO${FILE} "${SOURCE_FILE}"
 
 	if [ -s ${FILE} -a $(file -b --mime-type ${FILE}) != 'text/html' ]; then
 	    echo OK
 	    chmod -w ${FILE}
-	    LASTFILENAME="${FILENAME}"
 	    EXITCODE=0
 	else
 	    test -w ${FILE} && rm ${FILE}
 	    echo nok
-	    rm -f "$TMPFILE"
 	    exit ${EXITCODE}
 	fi
     fi
@@ -46,4 +55,3 @@ while true; do
 
 done
 
-rm -f "$TMPFILE"
