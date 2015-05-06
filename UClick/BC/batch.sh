5d85e0f291195cc00ea38ea99eda62822e4ad8f7
Christian Garbs <mitch@cgarbs.de>
2015-05-06 22:55:12 +0200

    remove BC as it does not seem to exist any more
    update dosage->mainline is impossible

--- UClick/BC/batch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# needs dosage from http://slipgate.za.net/dosage
-# Debian package available
-#
-mainline -b ../.. -c UClick/BC
-
-
--- UClick/BC/mkindex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ls *.gif | sort | while read FILE; do
-
-    echo -e "${FILE}\thttp://www.comics.com/creators/bc/archive/bc-20${FILE:4:6}.html\t[${FILE:8:2}.${FILE:6:2}.20${FILE:4:2}]"
-
-done > index
