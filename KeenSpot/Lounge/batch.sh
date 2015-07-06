3d2a73001322ce3b579b9712e62d5f6352b1ec22
Christian Garbs <mitch@cgarbs.de>
2015-07-06 23:00:31 +0200

    fix The Lounge for current dosage version
    manual intervention needed, see Lounge/batch.sh

--- KeenSpot/Lounge/batch.sh
+++ KeenSpot/Lounge/batch.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
-#
-# needs dosage from http://slipgate.za.net/dosage or Debian
-#
-mainline -b ../.. -c KeenSpot/Lounge
 
+exec 1>&2
+
+echo "KeenSpot/Lounge has moved to KeenSpot/TheLounge"
+echo "(it's a change in dosage that we have to accept...)"
+echo
+echo "1."
+echo "manually move all your stuff over to the new folder"
+echo "and switch your batch.sh/mkindex.sh calls over to"
+echo "that folder as well"
+echo
+echo "2."
+echo "rename all *.jpeg files to *.jpg, or they will be"
+echo "downloaded again as duplicates (again, this is a"
+echo "dosage change we have to accept)"
+echo
+echo "3."
+echo "afterwards a Scan for Comics should be issued in"
+echo "the web interface.  read/unread-counters should not"
+echo "be affected."
+
+exit 1
---
+++ KeenSpot/TheLounge/batch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# needs dosage from needs dosage from http://dosage.rocks/
+#
+dosage -b ../.. -c KeenSpot/TheLounge
+
