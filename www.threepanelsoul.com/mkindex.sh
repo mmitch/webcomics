e5c700638b85b9b9c4b64ce130c44f7caf251ef0
Christian Garbs <mitch@cgarbs.de>
2015-09-18 23:49:32 +0200

    fix new filenames in Three Panel Soul

--- www.threepanelsoul.com/mkindex.sh
+++ www.threepanelsoul.com/mkindex.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-ls | egrep '\.(gif|jpg|png)$' | sort | while read FILE; do
+(
+    ls | egrep '^[0-9]{4}-[0-9]{2}-.*\.(gif|jpg|png)$' | sort
+    ls | egrep '^[0-9]{10}-.*\.(gif|jpg|png)$' | sort
+) |  while read FILE; do
 
-    echo -e "${FILE}\t${FILE:8:2}.${FILE:5:2}.${FILE:0:4}"
+	 if [ ${FILE:4:1} = '-' ] ; then
+	     DATE=${FILE:8:2}.${FILE:5:2}.${FILE:0:4}
+	 else
+	     printf -v DATE '%(%d.%m.%Y)T' ${FILE:0:10}
+	 fi
 
-done > index
+	 echo -e "${FILE}\t${DATE}"
+
+    done > index
