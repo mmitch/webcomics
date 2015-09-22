ea5c4fe1a6c340b2d829802e2948fbb52364dee9
Christian Garbs <mitch@cgarbs.de>
2015-09-23 00:42:50 +0200

    fix scripts using Webcomic::Comicpress
    The latest fix to Three Panel Soul affected all Comicpress.pm based
    scripts - while fixing one script Soul, it broke at least three
    others…
    
    Make the fixes private to Three Panel Soul (use a unique Webcomic
    configuration instead of Webcomic::Comicpress) and revert the fix to
    Webcomic::Comicpress

--- Webcomic/Comicpress.pm
+++ Webcomic/Comicpress.pm
@@ -9,15 +9,15 @@ has '+tags' => ( builder => '_tags' );
 sub _tags {
     my $self = shift;
     return { 'div' => sub { my ($tag, $info) = @_;
-                            if ($tag->has_property('id', 'cc-comicbody')) {
+			    if ($tag->has_property('id', 'comic')) {
                                 $info->{'image'} = $tag->next_image();
-                            } elsif ($tag->has_property('id', 'nav') ||
-                                     $tag->has_property('class', 'prev')) {
+                            } elsif ($tag->has_property('id', 'navbar') ||
+                                     $tag->has_property('class', 'nav-previous')) {
                                 $info->{'next'} = $tag->next_link();
                             }
                         },
              'a' => sub { my ($tag, $info) = @_;
-                          if ($tag->has_property('class', 'prev', 1)) {
+                          if ($tag->has_property('class', 'navi-prev', 1)) {
                               $info->{'next'} = $tag->get_property('href');
                               unless ($info->{'next'} =~ /^http/) {
                                   $info->{'next'} = $self->url() . $info->{'next'};
--- www.threepanelsoul.com/batch.pl
+++ www.threepanelsoul.com/batch.pl
@@ -2,8 +2,27 @@
 
 use strict;
 use lib '..';
-use Webcomic::Comicpress;
+use Webcomic;
 
-my $comic = Webcomic::Comicpress->new(url => 'http://www.threepanelsoul.com/');
+my $comic = Webcomic->new(url => 'http://www.threepanelsoul.com/');
+
+$comic->tags({
+    'div' => sub { my ($tag, $info) = @_;
+		   if ($tag->has_property('id', 'cc-comicbody')) {
+		       $info->{'image'} = $tag->next_image();
+		   } elsif ($tag->has_property('id', 'nav') ||
+			    $tag->has_property('class', 'prev')) {
+		       $info->{'next'} = $tag->next_link();
+		   }
+    },
+    'a' => sub { my ($tag, $info) = @_;
+		 if ($tag->has_property('class', 'prev', 1)) {
+		     $info->{'next'} = $tag->get_property('href');
+		     unless ($info->{'next'} =~ /^http/) {
+			 $info->{'next'} = $comic->url() . $info->{'next'};
+		     }
+		 }
+    }
+	     });
 
 $comic->update();
