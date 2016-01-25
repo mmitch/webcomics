0c3b5bd41ce313c43e2df9c8e35c692cf9272f80
Christian Garbs <mitch@cgarbs.de>
2016-01-25 20:49:11 +0100

    fix xkcd
    skip 1608, it's a game
    try it anyway: http://xkcd.com/1608/

--- xkcd/batch.sh
+++ xkcd/batch.sh
@@ -18,7 +18,7 @@ while true; do
 
     # SKIP VERY SPECIAL COMICS
     case ${LATEST} in
-	1350|1416|1525)
+	1350|1416|1525|1608)
 	    echo "skipping ${LATEST}..."
 	    LATEST=$((${LATEST} + 1))
 	    continue
