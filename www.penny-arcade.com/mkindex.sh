d62db62834fe7ebe93579fa77201c377bc85db9e
Christian Garbs <mitch@cgarbs.de>
2017-03-04 23:02:13 +0100

    Penny Arcade: add online link to strip

--- www.penny-arcade.com/mkindex.sh
+++ www.penny-arcade.com/mkindex.sh
@@ -2,12 +2,14 @@
 
 ls *.[gjp][ipn][fg] | sort | while read FILE; do
 
+    LINE="${FILE}\thttps://www.penny-arcade.com/comic/${FILE:0:4}/${FILE:4:2}/${FILE:6:2}/\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
+
     TITLEFILE=${FILE:0:8}.txt
     if [ -s ${TITLEFILE} ] ; then
 	read TEXT < ${TITLEFILE}
-	echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}: ${TEXT}"
-    else
-	echo -e "${FILE}\t${FILE:6:2}.${FILE:4:2}.${FILE:0:4}"
+	LINE="$LINE: ${TEXT}"
     fi
+
+    echo -e "$LINE"
 
 done > index
