81d8d7dfc0dc379e98f9924387c2fd56a0a86e19
Christian Garbs <mitch@cgarbs.de>
2017-03-04 22:56:59 +0100

    remove CVS inline log

--- www.penny-arcade.com/mkindex.sh
+++ www.penny-arcade.com/mkindex.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# $Log: mkindex.sh,v $
-# Revision 1.3  2003-02-08 20:09:58  mitch
-# Datum wird im Titel mit angzeigt
-#
-# Revision 1.2  1997/01/04 06:55:06  mitch
-# Titel übernehmen, wenn vorhanden
-#
-# Revision 1.1  1997/01/04 02:32:45  mitch
-# Initial source import.
-# Kopierbasis: www.errantstory.com/mkindex.sh Rev. 1.1
-#
-
 ls *.[gjp][ipn][fg] | sort | while read FILE; do
 
     TITLEFILE=${FILE:0:8}.txt
