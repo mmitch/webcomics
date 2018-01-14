c63097bef816f02017f1d2e8db27b558d0ff34a8
Christian Garbs <mitch@cgarbs.de>
2018-01-14 20:53:58 +0100

    Spinerette: fix index generation
    there are two completely different file name schemes, support both

--- www.spinnyverse.com/mkindex.sh
+++ www.spinnyverse.com/mkindex.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-find . -name \*.jpg | sed -e 's,^\./,,' | sort -n | while read -r FILE; do
+exec > index
+
+# older stuff -> filename starts with YYYY-MM-DD
+
+find . -name '????-??-??*.jpg' | sed -e 's,^\./,,' | sort | while read -r FILE; do
+
+    printf -v DATE '%s.%s.%s' "${FILE:8:2}" "${FILE:5:2}" "${FILE:0:4}"
+    printf '%s\t%s\n' "$FILE" "$DATE"
+
+done
+
+
+# newer stuff -> filename starts with Unix-Timestamp
+
+find . -name '????[0-9]*.jpg' | sed -e 's,^\./,,' | sort -n | while read -r FILE; do
 
     TSP="${FILE%%-*}"
     printf '%s\t%(%d.%m.%Y)T\n' "$FILE" "$TSP"
 
-done > index
+done
