3541a54c434b4fb9b1d9f3194064eb4e65ac91d2
Christian Garbs <mitch@cgarbs.de>
2018-09-09 23:53:12 +0200

    whitelist broken/ended comics
    first batch: not updated >999 days

--- stallcheck.pl
+++ stallcheck.pl
@@ -10,7 +10,26 @@ use strict;
 # whitelist
 my %whitelist = map { $_ => $_ } qw(
 
+BetterDays
 COMIC
+ComicsDotCom/arlonjanis
+DominicDeegan
+Drowtales
+Flipside
+IrregularWebcomic
+KeenSpot/Ghastly
+KeenSpot/RaveKitty
+KeenSpot/SinFest
+KeenSpot/TwoLumps
+Nodwick
+RealLife
+SequentialArt
+SnafuComics/GrimTales
+SnafuComics/PowerPuffGirls
+Sokora
+UClick/BC
+VGButts
+VGCats/super
 archive/1994/www.dilbert.com
 archive/1995/www.dilbert.com
 archive/1996/www.dilbert.com
@@ -59,28 +78,48 @@ archive/2010/www.legostargalactica.net
 archive/2010/www.pvponline.com
 archive/IrregularWebcomic
 archive/www.applegeeks.com
+digitalunrestcomic.com
 inverloch.seraph-inn.com
 manga.clone-army.org
 manga.clone-army.org/april_and_may
 manga.clone-army.org/hh
 manga.clone-army.org/june_in_summer
 manga.clone-army.org/momoka_corner
+manga.clone-army.org/nana
 manga.clone-army.org/paper_eleven
 manga.clone-army.org/penny_tribute
+requiem.seraph-inn.com
+satwcomic.com
 tsunamichan.keenspace.com/anime_parody
 tsunamichan.keenspace.com/experimental_comic_kotone
 tsunamichan.keenspace.com/guest_strips
 tsunamichan.keenspace.com/katwalk_studio
 tsunamichan.keenspace.com/magical_mina
+woweh.com
 www.alpha-shade.com
+www.applegeeks.com
+www.applegeeks.com/lite
+www.dorktower.com
 www.exploitationnow.com
+www.giantitp.com/erfworld
+www.giantitp.com/oots
 www.girlgeniusonline.com
+www.jaynaylor.com/originallife
 www.machall.com
+www.mezzacotta.net/garfield
 www.michael-fredrich.de
+www.mimisgrotto.com/thorn
+www.misfile.com
+www.nichtlustig.de
 www.pbfcomics.com
+www.pvponline.com
 www.queenofwands.net
 www.ruthe.de
+www.shortpacked.com
+www.upevil.com
+www.userfriendly.org
 www.w00t-comic.net
+yafgc.shipsinker.com
 );
 
 
