bb4825021278289d72ab9cdc4ddb8ad7628909ff
Christian Garbs <mitch@cgarbs.de>
2021-06-14 20:12:58 +0200

    inverloch: fix ShellCheck warnings

--- inverloch.seraph-inn.com/batch.sh
+++ inverloch.seraph-inn.com/batch.sh
@@ -2,18 +2,18 @@
 
 EXITCODE=2
 
-LATEST=$(ls | egrep '[0-9]{3}.jpg' | tail -n 1 | cut -c 1-3 | sed 's/^0*//')
-if [ -z ${LATEST} ]; then
+LATEST=$(for file in *.jpg; do echo "$file"; done | grep -E '[0-9]{3}.jpg' | sort -n | tail -n 1 | cut -c 1-3 | sed 's/^0*//')
+if [ -z "${LATEST}" ]; then
     LATEST=1  # first strip ever
 fi
 
-echo reading from ${LATEST}
+echo "reading from ${LATEST}"
 
 PAGEBASE="http://inverloch.seraph-inn.com/viewcomic.php?page="
 PICBASE="http://inverloch.seraph-inn.com/pages/"
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 
-while [ $LATEST -lt 765 ]; do
+while [ "$LATEST" -lt 765 ]; do
 
     echo -n "fetching ${LATEST}: "
     
@@ -21,23 +21,23 @@ while [ $LATEST -lt 765 ]; do
 
     FILE="$(printf "%03d.jpg" "${LATEST}")"
 
-    if [ -e ${FILE} -a ! -w ${FILE} ]; then
+    if [ -e "${FILE}" ] && ! [ -w "${FILE}" ]; then
 	echo skipping
     else
 
-	wget --user-agent="${USERAGENT}" --referer=${HTMLURL} -qO"${FILE}" "${PICBASE}${LATEST}.jpg"
+	wget --user-agent="${USERAGENT}" --referer="${HTMLURL}" -qO"${FILE}" "${PICBASE}${LATEST}.jpg"
 	
-	if [ -s ${FILE} ]; then
+	if [ -s "${FILE}" ]; then
 	    echo OK
-	    chmod -w ${FILE}
+	    chmod -w "${FILE}"
 	    EXITCODE=0
 	else
-	    test -w ${FILE} && rm ${FILE}
+	    test -w "${FILE}" && rm "${FILE}"
 	    echo nok
 	    exit ${EXITCODE}
 	fi
     fi
     
-    LATEST=$((${LATEST} + 1))
+    LATEST=$(( LATEST + 1))
 
 done
--- inverloch.seraph-inn.com/mkindex.sh
+++ inverloch.seraph-inn.com/mkindex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ls *.jpg | sort | while read FILE; do
+for file in *.jpg; do echo "$file"; done | grep -E '[0-9]{3}.jpg' | sort -n | while read -r FILE; do
 
-    STRIPZERO=$(echo ${FILE:0:3} | sed 's/^0*//')
-    echo -e "${FILE}\thttp://inverloch.seraph-inn.com/viewcomic.php?page=${STRIPZERO}\t#${STRIPZERO}"
+    STRIPZERO=$((10#${FILE:0:3}))
+    echo -e "${FILE}\\thttp://inverloch.seraph-inn.com/viewcomic.php?page=${STRIPZERO}\\t#${STRIPZERO}"
 
 done > index
