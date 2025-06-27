edc5cd9fa25edf20473ee63046533546bf9330c2
Christian Garbs <mitch@cgarbs.de>
2025-06-27 23:35:38 +0200

    oglaf: switch to https
    If the download does not work, then existing URLs might have to be
    rewritten.  Try this (fixing the latest .url file should suffice, but
    why not fix them all):
    
    ```sh
    for I in *.url; do sed -i 's,^http://oglaf,https://www.oglaf,' $I; done
    ```

--- oglaf.com/batch.sh
+++ oglaf.com/batch.sh
@@ -5,7 +5,7 @@ EXITCODE=2
 LATEST=$(ls | grep -E '^[0-9]{6}\.url' | tail -n 1 | cut -c 1-6  | sed 's/^0*//')
 if [ -z "${LATEST}" ]; then
     LATEST=1  # first strip ever
-    NEXTURL=http://oglaf.com/cumsprite/
+    NEXTURL=https://www.oglaf.com/cumsprite/
 else
     LINK=$(printf %06d.url ${LATEST})
     read -r NEXTURL < "${LINK}"
@@ -13,7 +13,7 @@ fi
 
 echo reading from ${LATEST}
 
-PAGEBASE=http://oglaf.com
+PAGEBASE=https://www.oglaf.com
 USERAGENT="Mozilla/4.0 (compatible; MSIE 5.0; Linux) Opera 5.0  [en]"
 TMPFILE=tmp.html
 
