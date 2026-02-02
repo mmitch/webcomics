8d96c2b8437450cecb2b2a7d507f573764701906
Christian Garbs <mitch@cgarbs.de>
2026-02-02 22:08:41 +0100

    fix Dumbing of Age
    page layout has strayed from Comicpress.pm, so just include it directly

--- www.dumbingofage.com/batch.pl
+++ www.dumbingofage.com/batch.pl
@@ -2,8 +2,27 @@
 
 use strict;
 use lib '..';
-use Webcomic::Comicpress;
+use Webcomic;
 
-my $comic = Webcomic::Comicpress->new(url => 'http://www.dumbingofage.com/');
+my $comic = Webcomic->new(url => 'http://www.dumbingofage.com/');
+
+$comic->tags({ 'div' => sub { my ($tag, $info) = @_;
+			    if ($tag->has_property('id', 'comic')) {
+                                $info->{'image'} = $tag->next_image();
+                            } elsif ($tag->has_property('id', 'navbar') ||
+                                     $tag->has_property('class', 'nav-previous')) {
+                                $info->{'next'} = $tag->next_link();
+                            }
+                        },
+             'a' => sub { my ($tag, $info) = @_;
+                          if ($tag->has_property('class', 'navi-prev', 1)
+			      or $tag->has_property('class', 'previous-comic', 1)) {
+                              $info->{'next'} = $tag->get_property('href');
+                              unless ($info->{'next'} =~ /^http/) {
+                                  $info->{'next'} = $comic->url() . $info->{'next'};
+                              }
+                          }
+                      },
+	     });
 
 $comic->update();
